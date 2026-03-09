19a05a29249889af715da06426285174b84a19ef
Seyoung Park <seyoungpark@hanyang.ac.kr>
2026-03-10 00:28:16 +0900

    회전 방식 변경

--- Week0/UBall.h
+++ Week0/UBall.h
@@ -9,7 +9,6 @@ public:
 	static constexpr float MaxSpeed = 0.01f;
 	static constexpr float MaxSpeedBothThrusters = 0.0015f;
 	static constexpr float gravity = -0.000001f;
-	static constexpr float MaxAttractionForce = 0.001f;
 
 	static ID3D11Buffer* SphereVertexBuffer;
 	static UINT NumVerticesSphere;
@@ -34,6 +33,24 @@ public:
 	static bool bApplyGravity;
 	static bool bApplyAttraction;
 
+	static constexpr float MaxLinearSpeed = 0.010f;
+	static constexpr float MaxAngularSpeed = 0.0035f;
+
+	static constexpr float MaxSoleJetpackSpeed = 0.008f;
+	static constexpr float MaxBothJetpackSpeed = 0.010f;
+
+	static constexpr float SoleJetpackForce = 0.00006f;
+	static constexpr float BothJetpackForce = 0.00005f;
+
+	static constexpr float JetpackTorqueAmount = 0.00008f;
+	static constexpr float LinearDamping = 0.995f;
+	static constexpr float AngularDamping = 0.95f;
+	static constexpr float GravityForce = 0.000002f;
+
+	static constexpr float MaxAttractionForce = 0.001f;
+
+	float PendingTorque = 0.0f;
+
 public:
 	UBall();
 	~UBall();
@@ -55,9 +72,11 @@ public:
 	void UpdateRenderer(URenderer& renderer) override;
 	void HandleCollision(UPrimitive* other)override;
 	void D(const FVector3& v)override;
-	void ClampSpeed();
-	void ClampSpeed2(float maxSpeed);
+	/*void ClampSpeed();
+	void ClampSpeed2(float maxSpeed);*/
 	void Render(URenderer& renderer) override;
 	void ApplyAttraction(const FVector3& point, float strength);
 	void ApplyThrust(bool bLeftThruster, bool bRightThruster, float deltaTime);
+	void ApplyJetpackForce(float thrustAmount);
+	void LimitVelocities(float maxLinearSpeed);
 };
