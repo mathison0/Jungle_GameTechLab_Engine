e00699d80f9082788be1e7535145c10148192785
Park SangHyeok <tkdgurehf00@gmail.com>
2026-03-13 13:44:13 +0900

    Project Base

---
+++ Project_Week2/FVertex.h
@@ -0,0 +1,6 @@
+#pragma once
+struct FVertex
+{
+	float x, y, z;    // Position
+	float r, g, b, a; // Color
+};
---
+++ Project_Week2/main.cpp
@@ -0,0 +1,164 @@
+#include <windows.h>
+#include "URenderer.h"
+//#include "FVector.h"
+#include "Sphere.h"
+
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_internal.h"
+#include "ImGui/imgui_impl_dx11.h"
+#include "imGui/imgui_impl_win32.h"
+
+//// 1. Define the triangle vertices
+//struct FVertexSimple
+//{
+//	float x, y, z;    // Position
+//	float r, g, b, a; // Color
+//};
+
+class UPrimitive
+{
+public:
+	virtual FVector GetLocation() = 0;
+	virtual void Update(bool) = 0;
+	virtual float GetRadius() = 0;
+	virtual bool IsCollide(UPrimitive* other) = 0;
+	virtual ~UPrimitive() = default;
+};
+
+
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+// 각종 메시지를 처리할 함수
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, message, wParam, lParam))
+	{
+		return true;
+	}
+
+	switch (message)
+	{
+	case WM_DESTROY:
+		// Signal that the app should quit
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	WCHAR WindowClass[] = L"JungleWindowClass";
+	WCHAR Title[] = L"Game Tech Lab";
+	WNDCLASSW wndclass = { 0, WndProc, 0, 0, 0, 0, 0, 0, 0, WindowClass };
+
+	RegisterClassW(&wndclass);
+
+	// 1024 x 1024 크기에 윈도우 생성
+	HWND hWnd = CreateWindowExW(0, WindowClass, Title, WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 1024, 1024,
+		nullptr, nullptr, hInstance, nullptr);
+
+	URenderer	renderer;
+
+	// D3D11 생성하는 함수를 호출합니다.
+	renderer.Create(hWnd);
+	renderer.CreateShader();
+
+	renderer.CreateConstantBuffer();
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	ImGui_ImplWin32_Init((void*)hWnd);
+	ImGui_ImplDX11_Init(renderer.Device, renderer.DeviceContext);
+
+
+	UINT numVerticesSphere = sizeof(sphere_vertices) / sizeof(FVertex);
+	ID3D11Buffer* vertexBufferSphere = renderer.CreateVertexBuffer(sphere_vertices, sizeof(sphere_vertices));
+
+	bool bIsExit = false;
+	const int targetFPS = 60;
+	const double targetFrameTime = 1000.0 / targetFPS;
+
+	LARGE_INTEGER frequency;
+	QueryPerformanceFrequency(&frequency);
+
+	LARGE_INTEGER startTime, endTime;
+	double elapsedTime = 0.0;
+
+
+	// Main Loop (Quit Message가 들어오기 전까지 아래 Loop를 무한히 실행하게 됨)
+	while (bIsExit == false)
+	{
+		// 여기에 추가합니다.
+		// 루프 시작 시간 기록
+		QueryPerformanceCounter(&startTime);
+		MSG msg;
+
+		// 처리할 메시지가 더 이상 없을때 까지 수행
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			// 키 입력 메시지를 번역
+			TranslateMessage(&msg);
+
+			// 메시지를 적절한 윈도우 프로시저에 전달, 메시지가 위에서 등록한 WndProc 으로 전달됨
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT)
+			{
+				bIsExit = true;
+				break;
+			}
+		}
+
+		renderer.Prepare();
+		renderer.PrepareShader();
+
+		renderer.UpdateConstant(FVector(0.f, 0.f, 0.f));
+		renderer.RenderPrimitive(vertexBufferSphere, numVerticesSphere);
+
+		ImGui_ImplDX11_NewFrame();
+		ImGui_ImplWin32_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("Jungle Property Window");
+		ImGui::Text("Hello Jungle World!");
+
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+		renderer.SwapBuffer();
+
+		do
+		{
+			Sleep(0);
+
+			// 루프 종료 시간 기록
+			QueryPerformanceCounter(&endTime);
+
+			// 한 프레임이 소요된 시간 계산 (밀리초 단위로 변환)
+			elapsedTime = (endTime.QuadPart - startTime.QuadPart) * 1000.0 / frequency.QuadPart;
+
+		} while (elapsedTime < targetFrameTime);
+
+
+		////////////////////////////////////////////
+	}
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	//renderer.ReleaseVertexBuffer(vertexBufferSphere);
+	renderer.ReleaseConstantBuffer();
+	renderer.ReleaseShader();
+	renderer.Release();
+
+	return 0;
+}
+
