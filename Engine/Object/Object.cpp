3a5176d3a7aa70ebdbaa9763e8952be37ed93795
HanYoungSeo <108516412+persona6whenrelease@users.noreply.github.com>
2026-03-14 14:08:22 +0900

    Object constructor fix

--- Engine/Object/Object.cpp
+++ Engine/Object/Object.cpp
@@ -1,1 +1,18 @@
 #include "Object/Object.h"
+
+int32 UObject::GetTotalBytes()
+{
+    return UObject::TotalAllocationBytes;
+}
+
+int32 UObject::GetTotalCounts()
+{
+    return UObject::TotalAllocationCounts;
+}
+
+
+
+UObject::UObject(uint32 InUUID, size_t InObjectType) :UUID(InUUID), ObjectType(InObjectType) 
+{
+
+}
--- Engine/Object/Object.h
+++ Engine/Object/Object.h
@@ -3,8 +3,20 @@
 
 class ENGINE_API UObject
 {
-	UObject() = default;
+public: 
+	UObject(uint32 InUUID, size_t InObjectType) :UUID(InUUID), ObjectType(InObjectType);
 	virtual ~UObject() = default;
 	
+
+
+	static int32 GetTotalBytes();
+	static int32 GetTotalCounts();
+
+private:
+	inline static int32 TotalAllocationBytes = 0;
+	inline static int32 TotalAllocationCounts = 0;
+
+	size_t ObjectType;
+	uint32 UUID;
 };
 
