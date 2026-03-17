c9c6e8afa6fc395dda943aa177bc8fcc15fa39e6
상현 조 <sprtms9062@gmail.com>
2026-03-17 10:39:45 +0900

    Camera method and memeber function improved

--- Engine/Source/Component/CameraComponent.cpp
+++ Engine/Source/Component/CameraComponent.cpp
@@ -2,32 +2,45 @@
 
 UCameraComponent::UCameraComponent()
 {
+	bCanEverTick = true;
+	Camera = new CCamera();
 }
 
 void UCameraComponent::Tick(float DeltaTime)
 {
+	USceneComponent::Tick(DeltaTime);
+
+	//TODO : will be add CameraArm, shake and interpolation  
 }
 
 void UCameraComponent::MoveForward(float Value)
 {
+	Camera->MoveForward(Value);
 }
 
 void UCameraComponent::MoveRight(float Value)
 {
+	Camera->MoveRight(Value);
+
 }
 
 void UCameraComponent::MoveUp(float Value)
 {
+	Camera->MoveUp(Value);
+
 }
 
 void UCameraComponent::Rotate(float DeltaYaw, float DeltaPitch)
 {
+	Camera->Rotate(DeltaYaw, DeltaPitch);
 }
 
 void UCameraComponent::SetFov(float inFov)
 {
+	Camera->SetFOV(inFov);
 }
 
 void UCameraComponent::SetSpeed(float Inspeed)
 {
+	Camera->SetSpeed(Inspeed);
 }
