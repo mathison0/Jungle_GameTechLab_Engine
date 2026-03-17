0ebe67f9750ef3ccaae58bc42b0b42737e17c029
Minjun Jang <jmj0ok@gmail.com>
2026-03-17 11:08:20 +0900

    chore: adjust some project settings

--- Project_Week2/Serialization/Private/FPrimitiveRecordConverter.cpp
+++ Project_Week2/Serialization/Private/FPrimitiveRecordConverter.cpp
@@ -1,20 +1,48 @@
 #include "FPrimitiveRecordConverter.h"
 
+#include "FUObjectFactory.h"
+
+#include "Actor/ASphere.h"
+#include "Actor/ACube.h"
+#include "Actor/ATriangle.h"
+#include "Actor/ATorus.h"
+
+const TMap<FString, FString> FPrimitiveRecordConverter::ClassNameMap =
+{
+	{ "ASphere", "Sphere" },
+	{ "ACube", "Cube" },
+	{ "ATriangle", "Triangle" },
+	{ "ATorus", "Torus" }
+};
+
 FPrimitiveRecord FPrimitiveRecordConverter::FromActor(const AActor* Actor)
 {
 	FPrimitiveRecord Record;
 
-	Record.Type = Actor->GetName(); // 혹시 Actor의 메타데이터에서 Name이 그냥 클래스 이름이면 그거랑 매핑해서 좀 예쁜 이름으로 출력하도록...
+	const FString& ClassName = Actor->GetClass()->ClassName;
+
+	// JSON용 타입 이름 변환
+	const FString* JsonType = ClassNameMap.find(ClassName);
+
+	if (JsonType)
+	{
+		Record.Type = *JsonType;
+	}
+	else
+	{
+		// 매핑이 없으면 클래스 이름 그대로 사용
+		Record.Type = ClassName;
+	}
 
 	USceneComponent* SceneComponent = Actor->GetRootComponent();
-	Record.Location = SceneComponent->GetRelativeLocation(); // 일단은 상대 주소가 그대로 월드 주소로 가정하겠습니다.
-	Record.Rotation = SceneComponent->GetRelativeRotation();
-	Record.Scale = SceneComponent->GetRelativeScale();
+
+	if (SceneComponent)
+	{
+		// 현재 프로젝트에서는 relative == world 로 가정
+		Record.Location = SceneComponent->GetRelativeLocation();
+		Record.Rotation = SceneComponent->GetRelativeRotation();
+		Record.Scale = SceneComponent->GetRelativeScale();
+	}
 
 	return Record;
 }
-
-//AActor* FPrimitiveRecordConverter::ToActor(const FPrimitiveRecord& Record)
-//{
-//	// 팩토리 코드 완성 후 작성
-//}
--- Project_Week2/Serialization/Public/FPrimitiveRecordConverter.h
+++ Project_Week2/Serialization/Public/FPrimitiveRecordConverter.h
@@ -1,12 +1,18 @@
 #pragma once
 
-#include "FPrimitiveRecord.h"
+#include "CoreTypes.h"
+#include "FWorldSaveData.h"
 #include "Actor/AActor.h"
 #include "Component/USceneComponent.h"
 
 class FPrimitiveRecordConverter
 {
 public:
+
 	static FPrimitiveRecord FromActor(const AActor* Actor);
-	//static AActor* ToActor(const FPrimitiveRecord& Record);
+	static AActor* ToActor(const FPrimitiveRecord& Record);
+
+private:
+
+	static const TMap<FString, FString> ClassNameMap;
 };
