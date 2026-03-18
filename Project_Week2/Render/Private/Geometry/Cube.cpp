2927b13e55bb181ddc25530cab5a17652f0951e6
HyoBeom Kim <i1t28ly@gmail.com>
2026-03-18 21:08:38 +0900

    style: delete cube_index

--- Project_Week2/Render/Private/Geometry/Cube.cpp
+++ Project_Week2/Render/Private/Geometry/Cube.cpp
@@ -52,32 +52,4 @@ FVertexSimple cube_vertices[] =
 	{  0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.5f, 1.0f }, // Bottom-right (purple)
 };
 
-uint32 cube_indices[] = {
-	// Front face (Z+) - 버텍스 0~5
-	0, 1, 2,   // 삼각형 1
-	3, 4, 5,   // 삼각형 2
-
-	// Back face (Z-) - 버텍스 6~11
-	6, 7, 8,
-	9, 10, 11,
-
-	// Left face (X-) - 버텍스 12~17
-	12, 13, 14,
-	15, 16, 17,
-
-	// Right face (X+) - 버텍스 18~23
-	18, 19, 20,
-	21, 22, 23,
-
-	// Top face (Y+) - 버텍스 24~29
-	24, 25, 26,
-	27, 28, 29,
-
-	// Bottom face (Y-) - 버텍스 30~35
-	30, 31, 32,
-	33, 34, 35,
-};
-
-const size_t cube_index_count = sizeof(cube_indices) / sizeof(uint32);
-
 const size_t cube_vertex_count = sizeof(cube_vertices) / sizeof(FVertexSimple);
--- Project_Week2/Render/Public/Geometry/Cube.h
+++ Project_Week2/Render/Public/Geometry/Cube.h
@@ -5,6 +5,3 @@
 
 extern FVertexSimple cube_vertices[];
 extern const size_t cube_vertex_count;
-
-extern uint32 cube_indices[];
-extern const size_t cube_index_count;
