e89ee80db2f2253d29d6b383a49a6ebd90fe7309
jskim-research <wltnwkd21@gmail.com>
2026-03-22 14:13:47 +0900

    Fix: correct matrix multiplication order

--- Engine/Source/Component/PrimitiveComponent.cpp
+++ Engine/Source/Component/PrimitiveComponent.cpp
@@ -29,26 +29,25 @@ FBoxSphereBounds UPrimitiveComponent::GetWorldBoundsForAABB() const
 
 	if (MeshData)
 	{
+		FVector LocalBoxExtent = (MeshData->GetMaxCoord() - MeshData->GetMinCoord()) * 0.5;
 		Center = GetWorldTransform().TransformPosition(MeshData->GetCenterCoord());
 		FVector S = GetWorldTransform().GetScaleVector();
-		FVector ScaledExtent = FVector::Multiply((MeshData->GetMaxCoord() - MeshData->GetMinCoord()) * 0.5, S);
+		FVector ScaledExtent = FVector::Multiply(LocalBoxExtent, S);
 		FMatrix AbsR = FMatrix::Abs(GetWorldTransform().GetRotationMatrix());
 
 		FVector WorldBoxExtent;
-		WorldBoxExtent.X =
-			AbsR.M[0][0] * ScaledExtent.X +
-			AbsR.M[0][1] * ScaledExtent.Y +
-			AbsR.M[0][2] * ScaledExtent.Z;
+		// S * R (row-major)
+		WorldBoxExtent.X = AbsR.M[0][0] * ScaledExtent.X
+			+ AbsR.M[1][0] * ScaledExtent.Y 
+			+ AbsR.M[2][0] * ScaledExtent.Z;
 
-		WorldBoxExtent.Y =
-			AbsR.M[1][0] * ScaledExtent.X +
-			AbsR.M[1][1] * ScaledExtent.Y +
-			AbsR.M[1][2] * ScaledExtent.Z;
+		WorldBoxExtent.Y = AbsR.M[0][1] * ScaledExtent.X
+			+ AbsR.M[1][1] * ScaledExtent.Y
+			+ AbsR.M[2][1] * ScaledExtent.Z;
 
-		WorldBoxExtent.Z =
-			AbsR.M[2][0] * ScaledExtent.X +
-			AbsR.M[2][1] * ScaledExtent.Y +
-			AbsR.M[2][2] * ScaledExtent.Z;
+		WorldBoxExtent.Z = AbsR.M[0][2] * ScaledExtent.X
+			+ AbsR.M[1][2] * ScaledExtent.Y
+			+ AbsR.M[2][2] * ScaledExtent.Z;
 
 		return { Center, WorldBoxExtent.SizeSquared(), WorldBoxExtent };
 	}
