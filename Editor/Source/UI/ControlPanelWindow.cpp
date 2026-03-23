dee75b5211f27ccfc12d243fcadea02d9983c353
jskim-research <wltnwkd21@gmail.com>
2026-03-23 19:37:52 +0900

    Chore: add test environment

--- Editor/Source/UI/ControlPanelWindow.cpp
+++ Editor/Source/UI/ControlPanelWindow.cpp
@@ -16,6 +16,8 @@
 #include "Component/CameraComponent.h"
 #include "Controller/EditorViewportController.h"
 #include <filesystem>
+#include <random>
+#include <chrono>
 
 namespace
 {
@@ -89,8 +91,27 @@ void CControlPanelWindow::Render(CCore* Core)
 			ImGui::TextUnformatted("Preview scene is editor-only. Scene save/load is disabled.");
 		}
 		ImGui::SeparatorText("Camera");
-		
 
+		if (ImGui::Button("Spawn Test"))
+		{
+			UScene* Scene = Core->GetScene();
+			AActor* NewActor = nullptr;
+
+			for (int i = 0; i < 1000; i++)
+			{
+				// 시드: 현재 시간 기반
+				static std::mt19937 rng(static_cast<unsigned int>(
+					std::chrono::steady_clock::now().time_since_epoch().count()
+					));
+
+				std::uniform_real_distribution<float> dist(-10, 10);
+
+				FVector V{ dist(rng), dist(rng), dist(rng) };
+				NewActor = Scene->SpawnActor<ACubeActor>("Test");
+				NewActor->SetActorLocation(V);
+			}
+		}
+		
 		if (CCamera* Camera = Core->GetScene()->GetCamera())
 		{
 		
