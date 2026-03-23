20ad028bd8466a8b5256cf886d6560d5ff20d2b4
jskim-research <wltnwkd21@gmail.com>
2026-03-24 08:40:07 +0900

    Feat: add mouse drag, drop, file move, delete

--- Editor/Source/UI/ContentBrowserWindow.h
+++ Editor/Source/UI/ContentBrowserWindow.h
@@ -17,17 +17,29 @@ public:
 	void SetFileIcon(ID3D11ShaderResourceView* FileSRV);
 
 	std::function<void(const FString& FilePath)> OnFileDoubleClickCallback;
+	std::function<void(const FString& DraggingFilePath, const FString& ReleaseDirectory)> OnFileDragEnd;
+
+	bool IsHovered() const { return bIsHovered; }
+	bool IsMouseOnDirectory() const { return bIsMouseOnDirectory; }
+	bool IsMouseOnFile() const { return bIsMouseOnFile; }
 
 private:
 	std::filesystem::path RootPath;
 	std::filesystem::path CurrentPath;
 	std::filesystem::path SelectedPath;
+	// Dragging 대상
+	std::filesystem::path SelectedFilePath;
 
-	/*
-		ID3D11ShaderResourceView* FolderSRV;
-		ImTextureID FolderIcon = (ImTextureID)FolderSRV;
-	*/
-	ImTextureID FolderIcon;
+	std::filesystem::path DirectoryPathUnderMouse;
+	std::filesystem::path FilePathUnderMouse;
+
+	bool bFileOnDrag = false;
+	bool bIsHovered = false;
+
+	bool bIsMouseOnDirectory = false;
+	bool bIsMouseOnFile = false;
+
+	ImTextureID FolderIcon;  /* (ImTextureID)FolderSRV */
 	ImTextureID FileIcon;
 
 	void DrawFolderTree(const std::filesystem::path& Path);
