8d1fa6702c6be049494504c53d9266a0a2175cdd
상현 조 <sprtms9062@gmail.com>
2026-03-25 13:33:49 +0900

    feat:: skysphere init

--- Engine/Source/Actor/SkySphereActor.h
+++ Engine/Source/Actor/SkySphereActor.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "Actor.h"
+
+
+class USkyComponent;
+class ENGINE_API ASkySphereActor : public AActor
+{
+public:
+	static UClass* StaticClass();
+
+	ASkySphereActor(UClass* InClass, const FString& InName, UObject* InOuter = nullptr);
+
+	void PostSpawnInitialize() override;
+	void Tick(float DeltaTime) override;
+	USkyComponent* GetSkyComponent() const { return SkyComponent; }
+
+private:
+	USkyComponent* SkyComponent = nullptr;
+};
