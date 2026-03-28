c4f4ba981fff8412c9c0a4d3b310c6fcfac2ea48
keonwookang0914 <kwkang0914@gmail.com>
2026-03-28 17:06:39 +0900

    fix: 누락된 반환자 + 의존성 문제 주석 추가

--- W4_Jungle_Team2/Source/Engine/Runtime/SceneView.h
+++ W4_Jungle_Team2/Source/Engine/Runtime/SceneView.h
@@ -15,7 +15,8 @@ struct FSceneView
 	FVector CameraRight;
 	FVector CameraUp;
 
-
+	// TODO : Editor에서 Engine을 참조하는 의존성 문제로 임시 주석 처리
+	// 다른 곳에서 정보를 가지고있어야함. EditorViewportClient면 딱 좋을 것 같은데?
 	// EEditorCameraMode CameraMode;
 	// EEditorViewMode ViewMode;
 
--- W4_Jungle_Team2/Source/Engine/Slate/SViewport.cpp
+++ W4_Jungle_Team2/Source/Engine/Slate/SViewport.cpp
@@ -2,32 +2,32 @@
 
 bool SViewport::OnMouseMove(int X, int Y)
 {
-	
+	return false;
 }
 
 bool SViewport::OnMouseButtonDown(int Button, int X, int Y)
 {
-	
+	return false;
 }
 
 bool SViewport::OnMouseButtonUp(int Button, int X, int Y)
 {
-	
+	return false;
 }
 
 bool SViewport::OnMouseWheel(int Delta, int X, int Y)
 {
-	
+	return false;
 }
 
 bool SViewport::OnKeyDown(uint32 Key)
 {
-	
+	return false;
 }
 
 bool SViewport::OnKeyUp(uint32 Key)
 {
-	
+	return false;
 }
 
 FViewportMouseEvent SViewport::MakeMouseEvent(int X, int Y) const
