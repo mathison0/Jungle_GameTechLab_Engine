6184dbfb8fe2bcc4b296eb5e410924f8e27a7705
keonwookang0914 <kwkang0914@gmail.com>
2026-03-28 17:01:31 +0900

    feat: Viewport, ViewportClient, MessageHandler 등 추상클래스 + 렌더링에 필요한 정보를 담고있는 SceneView 구조체 추가

---
+++ W4_Jungle_Team2/Source/Engine/Runtime/SceneView.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Math/Matrix.h"
+#include "Slate/SlateUtils.h"
+
+struct FSceneView
+{
+	FViewportRect ViewRect;
+
+	FMatrix ViewMatrix;
+	FMatrix ProjectionMatrix;
+	FMatrix ViewProjectionMatrix;
+
+	FVector CameraPosition;
+	FVector CameraForward;
+	FVector CameraRight;
+	FVector CameraUp;
+
+
+	// EEditorCameraMode CameraMode;
+	// EEditorViewMode ViewMode;
+
+	bool bOrthographic = false;
+};
+
---
+++ W4_Jungle_Team2/Source/Engine/Runtime/Viewport.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "Slate/SlateUtils.h"
+/*
+* Viewport 가장 Base 클래스
+* Rect / Local coordinate Helper
+* Common Viewport Utility
+*/
+class FViewport
+{
+public:
+	virtual ~FViewport() = default;
+
+	virtual void SetRect(const FViewportRect& InRect) { Rect = InRect; }
+	const FViewportRect& GetRect() const { return Rect; }
+
+	bool ContainsPoint(int X, int Y) const { return Rect.Contains(X, Y); }
+	void WindowToLocal(int X, int Y, int& OutX, int& OutY) const 
+	{ 
+		return Rect.WindowToLocal(X, Y, OutX, OutY); 
+	}
+
+protected:
+	FViewportRect Rect;
+};
+
+
