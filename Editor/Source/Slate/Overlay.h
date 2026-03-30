5ab7017ffe1d07ced2e6e37d78dab64cc37b9d09
Chanil-Chong <chan450471@gmail.com>
2026-03-30 16:38:18 +0900

    Add overlay slate

---
+++ Editor/Source/Slate/Overlay.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Panel.h"
+
+class SOverlay : public SPanel
+{
+public:
+	FSlot& AddSlot() { Slots.push_back({}); return Slots.back(); }
+
+	FVector2 ComputeDesiredSize() const override;
+	void ArrangeChildren() override;
+};
