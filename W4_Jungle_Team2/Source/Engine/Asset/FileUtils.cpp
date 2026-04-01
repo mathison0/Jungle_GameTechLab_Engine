8529b2e715c497de7f02550946cb6c1478b591b3
alogeclock <alogeclock@gmail.com>
2026-04-01 14:44:39 +0900

    Feat: 루트 경로와 검색할 파일명을 입력하면, 검색한 파일의 절대 경로가 저장되는 함수

--- W4_Jungle_Team2/Source/Engine/Asset/FileUtils.cpp
+++ W4_Jungle_Team2/Source/Engine/Asset/FileUtils.cpp
@@ -1,4 +1,5 @@
 ﻿#include "FileUtils.h"
+#include "Core/Paths.h"
 
 #include <fstream>
 #include <filesystem>
@@ -49,4 +50,33 @@ bool FFileUtils::LoadFileToLines(const FString& FileName, TArray<FString>& OutLi
 	}
 	
 	return true;
+}
+
+// 하위 폴더를 검색하여 타겟 파일의 전체(또는 상대) 경로를 찾는 함수
+
+/* [사용 예시] FoundPath에 "Asset/Mesh/Nature/Lowpoly tree.mtl" 같은 실제 경로가 들어오게 됩니다.
+FString FoundPath;
+FFileUtils::FindFileRecursively("Asset", "Lowpoly tree.mtl", FoundPath)
+
+*/
+bool FFileUtils::FindFileRecursively(const FString& SearchRootPath, const FString& TargetFileName, FString& OutFoundPath)
+{
+	std::filesystem::path RootPath = FPaths::ToWide(SearchRootPath);
+	std::filesystem::path TargetName = FPaths::ToWide(TargetFileName);
+
+	if (!std::filesystem::exists(RootPath) || !std::filesystem::is_directory(RootPath))
+	{
+		return false;
+	}
+
+	for (const auto& Entry : std::filesystem::recursive_directory_iterator(RootPath))
+	{
+		if (Entry.is_regular_file() && Entry.path().filename() == TargetName)
+		{
+			OutFoundPath = FPaths::ToUtf8(Entry.path().generic_wstring());
+			return true;
+		}
+	}
+
+	return false;
 }
