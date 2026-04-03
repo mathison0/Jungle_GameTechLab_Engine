2aae03f1e829c520fc81baf4a3f5ccdc5a3e71b9
Seyoung Park <seyoungpark@hanyang.ac.kr>
2026-04-03 20:35:17 +0900

    feat: Implement frustum culling without visibility cache

--- PerformanceEngine/Source/Visibility/VisibilitySystem.cpp
+++ PerformanceEngine/Source/Visibility/VisibilitySystem.cpp
@@ -2,6 +2,9 @@
 
 #include "Camera/Camera.h"
 #include "Scene/Scene.h"
+#include "StaticMesh/StaticMesh.h"
+#include "Math/MathUtility.h"
+#include "Math/Vector.h"
 
 void FVisibilitySystem::Reset()
 {
@@ -10,15 +13,133 @@ void FVisibilitySystem::Reset()
 
 void FVisibilitySystem::Build(const FScene& InScene, const FCamera& InCamera, FVisibilityResults& OutResults)
 {
-	(void)InCamera;
-
 	const TArray<FRenderItem>& RenderItems = InScene.GetRenderItems();
 	OutResults.FrameNumber = NextFrameNumber++;
 	OutResults.VisiblePrimitiveIndices.clear();
 	OutResults.VisiblePrimitiveIndices.reserve(RenderItems.size());
 
+	const FFrustum Frustum = BuildFrustum(InCamera);
 	for (uint32 PrimitiveIndex = 0; PrimitiveIndex < static_cast<uint32>(RenderItems.size()); ++PrimitiveIndex)
 	{
-		OutResults.VisiblePrimitiveIndices.push_back(PrimitiveIndex);
+		const FRenderItem& Item = RenderItems[PrimitiveIndex];
+
+		if (!Item.StaticMesh || !Item.StaticMesh->IsValid())
+		{
+			continue; // 정적 메시가 없는 경우 건너뜀
+		}
+
+		if (IntersectsAABB(Frustum, Item.WorldBoundsMin, Item.WorldBoundsMax))
+		{
+			OutResults.VisiblePrimitiveIndices.push_back(PrimitiveIndex);
+		}
+	}
+
+	// 디버그 출력
+	const uint32 VisibleCount = static_cast<uint32>(OutResults.VisiblePrimitiveIndices.size());
+	const uint32 TotalCount = static_cast<uint32>(RenderItems.size());
+
+	std::string Msg = "Frustum Culling - Total: " + std::to_string(TotalCount)
+		+ ", Visible: " + std::to_string(VisibleCount) + "\n";
+
+	OutputDebugStringA(Msg.c_str());
+}
+
+namespace
+{
+	FPlane MakePlaneFromPointNormal(const FVector& Point, const FVector& Normal);
+	FPlane MakePlaneFromPoints(const FVector& PointA, const FVector& PointB, const FVector& PointC, const FVector& InFrustumCenter);
+}
+
+FFrustum FVisibilitySystem::BuildFrustum(const FCamera& InCamera) const
+{
+	FFrustum Frustum;
+
+	const FTransform& CameraTransform = InCamera.GetTransform();
+	const FVector CameraPos = CameraTransform.GetLocation();
+	const FVector Forward = CameraTransform.GetUnitAxis(EAxis::X).GetSafeNormal();
+	const FVector Right = CameraTransform.GetUnitAxis(EAxis::Y).GetSafeNormal();
+	const FVector Up = CameraTransform.GetUnitAxis(EAxis::Z).GetSafeNormal();
+
+	const float NearDist = InCamera.GetNearClip();
+	const float FarDist = InCamera.GetFarClip();
+	const float HalfFovRad = FMath::DegreesToRadians(InCamera.GetFOV() * 0.5f);
+
+	const float NearHalfHeight = std::tan(HalfFovRad) * NearDist;
+	const float NearHalfWidth = NearHalfHeight * InCamera.GetAspectRatio();
+
+	const float FarHalfHeight = std::tan(HalfFovRad) * FarDist;
+	const float FarHalfWidth = FarHalfHeight * InCamera.GetAspectRatio();
+
+	const FVector NearCenter = CameraPos + Forward * NearDist;
+	const FVector FarCenter = CameraPos + Forward * FarDist;
+	const FVector FrustumCenter = CameraPos + Forward * ((NearDist + FarDist) * 0.5f);
+
+	const FVector NTL = NearCenter + Up * NearHalfHeight - Right * NearHalfWidth;
+	const FVector NTR = NearCenter + Up * NearHalfHeight + Right * NearHalfWidth;
+	const FVector NBL = NearCenter - Up * NearHalfHeight - Right * NearHalfWidth;
+	const FVector NBR = NearCenter - Up * NearHalfHeight + Right * NearHalfWidth;
+
+	/*const FVector FTL = FarCenter + Up * FarHalfHeight - Right * FarHalfWidth;
+	const FVector FTR = FarCenter + Up * FarHalfHeight + Right * FarHalfWidth;
+	const FVector FBL = FarCenter - Up * FarHalfHeight - Right * FarHalfWidth;
+	const FVector FBR = FarCenter - Up * FarHalfHeight + Right * FarHalfWidth;*/
+
+	// inward normals
+	Frustum.Planes[0] = MakePlaneFromPointNormal(NearCenter, Forward);     // near
+	Frustum.Planes[1] = MakePlaneFromPointNormal(FarCenter, -Forward);     // far
+
+	Frustum.Planes[2] = MakePlaneFromPoints(CameraPos, NTL, NBL, FrustumCenter); // left
+	Frustum.Planes[3] = MakePlaneFromPoints(CameraPos, NBR, NTR, FrustumCenter); // right
+	Frustum.Planes[4] = MakePlaneFromPoints(CameraPos, NTR, NTL, FrustumCenter); // top
+	Frustum.Planes[5] = MakePlaneFromPoints(CameraPos, NBL, NBR, FrustumCenter); // bottom
+
+	return Frustum;
+}
+
+bool FVisibilitySystem::IntersectsAABB(const FFrustum& InFrustum, const FVector& InBoxMin, const FVector& InBoxMax) const
+{
+	for (const FPlane& Plane : InFrustum.Planes)
+	{
+		FVector PositiveVertex = InBoxMin;
+		if (Plane.Normal.X >= 0) PositiveVertex.X = InBoxMax.X;
+		if (Plane.Normal.Y >= 0) PositiveVertex.Y = InBoxMax.Y;
+		if (Plane.Normal.Z >= 0) PositiveVertex.Z = InBoxMax.Z;
+
+		if (Plane.GetSignedDistanceToPoint(PositiveVertex) < 0)
+		{
+			return false; // AABB is completely outside this plane
+		}
+	}
+	return true; // AABB intersects or is inside the frustum
+}
+
+
+// 평면 생성 헬퍼
+namespace
+{
+	FPlane MakePlaneFromPointNormal(const FVector& Point, const FVector& Normal)
+	{
+		FPlane Plane;
+		Plane.Normal = Normal.GetSafeNormal();
+		Plane.Distance = -FVector::DotProduct(Plane.Normal, Point);
+		return Plane;
+	}
+
+	// 법선 방향이 프러스텀 중심을 향하도록.
+	FPlane MakePlaneFromPoints(const FVector& PointA, const FVector& PointB, const FVector& PointC, const FVector& InFrustumCenter)
+	{
+		FVector Normal = FVector::CrossProduct(PointB - PointA, PointC - PointA).GetSafeNormal();
+
+		FPlane Plane;
+		Plane.Normal = Normal;
+		Plane.Distance = -FVector::DotProduct(Plane.Normal, PointA);
+
+		if (Plane.GetSignedDistanceToPoint(InFrustumCenter) < 0.0f)
+		{
+			Plane.Normal *= -1.0f;
+			Plane.Distance *= -1.0f;
+		}
+
+		return Plane;
 	}
 }
--- PerformanceEngine/Source/Visibility/VisibilitySystem.h
+++ PerformanceEngine/Source/Visibility/VisibilitySystem.h
@@ -2,9 +2,26 @@
 
 #include "Types/Array.h"
 #include "Types/PlatformTypes.h"
+#include "Math/Vector.h"
 
 class FCamera;
 class FScene;
+
+struct FPlane
+{
+	FVector Normal = FVector::ForwardVector;
+	float Distance = 0.0f;
+
+	float GetSignedDistanceToPoint(const FVector& Point) const
+	{
+		return FVector::DotProduct(Normal, Point) + Distance;
+	}
+};
+
+struct FFrustum
+{
+	FPlane Planes[6];
+};
 
 struct FVisibilityResults
 {
@@ -17,6 +34,10 @@ class FVisibilitySystem
 public:
 	void Reset();
 	void Build(const FScene& InScene, const FCamera& InCamera, FVisibilityResults& OutResults);
+
+private:
+	FFrustum BuildFrustum(const FCamera& InCamera) const;
+	bool IntersectsAABB(const FFrustum& InFrustum, const FVector& InBoxMin, const FVector& InBoxMax) const;
 
 private:
 	uint64 NextFrameNumber = 1;
