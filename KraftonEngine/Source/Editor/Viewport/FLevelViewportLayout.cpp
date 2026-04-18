92bfad463a113a4cae3f6a2ea207559fa3dd5e4e
Yonaim <yonaimyo@gmail.com>
2026-04-18 10:56:46 +0900

    Feat: Editor UI에 뷰모드 변경 버튼 추가

--- KraftonEngine/Source/Editor/Viewport/FLevelViewportLayout.cpp
+++ KraftonEngine/Source/Editor/Viewport/FLevelViewportLayout.cpp
@@ -866,7 +866,11 @@ void FLevelViewportLayout::RenderPaneToolbar(int32 SlotIndex)
 				// View Mode
 				ImGui::Text("View Mode");
 				int32 CurrentMode = static_cast<int32>(Opts.ViewMode);
-				ImGui::RadioButton("Lit", &CurrentMode, static_cast<int32>(EViewMode::Lit));
+				ImGui::RadioButton("Lit_Gouraud", &CurrentMode, static_cast<int32>(EViewMode::Lit_Gouraud));
+				ImGui::SameLine();
+				ImGui::RadioButton("Lit_Lambert", &CurrentMode, static_cast<int32>(EViewMode::Lit_Lambert));
+				ImGui::SameLine();
+				ImGui::RadioButton("Lit_Phong", &CurrentMode, static_cast<int32>(EViewMode::Lit_Phong));
 				ImGui::SameLine();
 				ImGui::RadioButton("Unlit", &CurrentMode, static_cast<int32>(EViewMode::Unlit));
 				ImGui::SameLine();
