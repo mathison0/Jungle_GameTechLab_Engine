53df05b6abcd9384b8240060207693a20be01f79
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-04-21 22:47:03 +0900

    Revert "Merge branch 'feature/decal-overlap'"
    This reverts commit bb4f91a80a1274557e67866f43a3b24d4bbc873f, reversing
    changes made to 1c1a35f2df79352f5aea4c6c2ca093347a6304a2.

--- NipsEngine/Shaders/ShaderDecal.hlsl
+++ NipsEngine/Shaders/ShaderDecal.hlsl
@@ -104,11 +104,7 @@ PSOutput mainPS(PSInput input) : SV_Target
     uint2 tileData = TileBuffer[tileCoord.y * numTilesX + tileCoord.x];
 
 #if LIGHTING_MODEL_GOURAUD
-    float4 finalColor = decalTex * DecalColorTint;
-    if (finalColor.a < 0.001) {
-        discard;
-    }
-    output.Color = float4(input.LitColor, 1.0f) * finalColor;
+    output.Color = float4(input.LitColor, 1.0f) * decalTex * DecalColorTint;
     output.Normal = float4(input.WorldNormal * 0.5f + 0.5f, 1.f);
     output.WorldPos = float4(input.WorldPos, 1.0f);
     return output;
@@ -127,11 +123,8 @@ PSOutput mainPS(PSInput input) : SV_Target
             CalcSpotlightLambert(light, float3(1.0f, 1.0f, 1.0f), N_Lambert, input.WorldPos)
             : CalcPointLambert(light, float3(1.0f, 1.0f, 1.0f), N_Lambert, input.WorldPos);
     }
-    float4 finalColor = decalTex * DecalColorTint;
-    if (finalColor.a < 0.001) {
-        discard;
-    }
-    output.Color = finalColor * float4(accumulatedLight, 1.0f);
+    
+    output.Color = decalTex * DecalColorTint * float4(accumulatedLight, 1.0f);
     output.Normal = float4(N_Lambert * 0.5f + 0.5f, 1.f);
     output.WorldPos = float4(input.WorldPos, 1.0f);
     return output;
@@ -150,11 +143,8 @@ PSOutput mainPS(PSInput input) : SV_Target
             CalcSpotlightBlinnPhong(light, float3(1.0f, 1.0f, 1.0f), N_Phong, input.WorldPos, CameraPosition - input.WorldPos, 32.0f)
             : CalcPointBlinnPhong(light, float3(1.0f, 1.0f, 1.0f), N_Phong, input.WorldPos, CameraPosition - input.WorldPos, 32.0f);
     }
-    float4 finalColor = decalTex * DecalColorTint;
-    if (finalColor.a < 0.001) {
-        discard;
-    }
-    output.Color = finalColor * float4(accumulatedLight, 1.0f);
+    
+    output.Color = float4(accumulatedLight, 1.0f) * decalTex * DecalColorTint;
     output.Normal = float4(N_Phong * 0.5f + 0.5f, 1.f);
     output.WorldPos = float4(input.WorldPos, 1.0f);
     return output;
