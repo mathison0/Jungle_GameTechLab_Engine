ae13adee62d65ee45d781a479a88836719b95210
Seyoung Park <seyoungpark@hanyang.ac.kr>
2026-04-24 17:07:29 +0900

    add LightComponent/LightComponentBase member variable

--- KraftonEngine/Source/Engine/Component/Light/LightComponent.h
+++ KraftonEngine/Source/Engine/Component/Light/LightComponent.h
@@ -6,4 +6,17 @@ class ULightComponent : public ULightComponentBase
 public:
 	DECLARE_CLASS(ULightComponent, ULightComponentBase)
 
+	float GetShadowResolutionScale() const { return ShadowResolutionScale; }
+	float GetShadowBias() const { return ShadowBias; }
+	float GetShadowSlopeBias() const { return ShadowSlopeBias; }
+	float GetShadowSharpen() const { return ShadowSharpen; }
+
+	virtual void GetEditableProperties(TArray<FPropertyDescriptor>& OutProps) override;
+	virtual void Serialize(FArchive& Ar) override;
+
+protected:
+	float ShadowResolutionScale = 1.0f;
+	float ShadowBias = 0.01f;
+	float ShadowSlopeBias = 0.01f;
+	float ShadowSharpen = 0.0f;
 };
