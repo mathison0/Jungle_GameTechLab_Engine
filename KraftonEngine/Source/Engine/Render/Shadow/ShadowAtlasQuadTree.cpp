b60fdd9569b4b8c98d2d491435a81db599c0ef86
Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
2026-04-27 20:12:07 +0900

    Squashed commit of the following:
    commit b820b208d8bccc5f7e8bcd671a62f4d0a8ffdaa6
    Author: Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
    Date:   Mon Apr 27 20:10:30 2026 +0900
    
        Squashed commit of the following:
    
        commit 7b372ec24fda554a9bc07e15c67f381a2e7cf86d
        Author: LEE SangHoon <hoon15x@gmail.com>
        Date:   Mon Apr 27 19:37:32 2026 +0900
    
            Style: add comments to CSM view,proj
    
            CSM에서 cascade 하나에 대한 view, projection을 구하는 함수에 주석을 추가합니다
    
        commit a4422be761bba2a3f59e44675edd7406b3c7d94e
        Author: lin-ion <lin-ion@naver.com>
        Date:   Mon Apr 27 17:10:41 2026 +0900
    
            fix: update point light vsm use to 2darray
    
        commit 8111fd9332d20d1da1051d818d363e57bf9b952c
        Author: LEE SangHoon <hoon15x@gmail.com>
        Date:   Mon Apr 27 16:57:32 2026 +0900
    
            fix: no Icon transformation
    
            빛, 데칼 등에 달리는 아이콘만 잘못 픽킹해서 아이콘만 움직이는 사태 방지
    
    commit 8c5ce81e80feaf0521f69950b07f6337baac1ede
    Author: Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
    Date:   Mon Apr 27 20:09:53 2026 +0900
    
        Add: ECubeMapOrientation enum to GlobalLightParams
        Fix: AtlasQuadTreePoint compatibility with CMO enum
    
    commit e075a501d66fdfb1639e56a9bc0e91c26e4bf0a8
    Author: Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
    Date:   Mon Apr 27 19:42:30 2026 +0900
    
        Fix: Move z-guard as a const member
    
    commit fa880dffbe7df9acd98bc56b880fedac6f53b854
    Author: Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
    Date:   Mon Apr 27 19:40:16 2026 +0900
    
        Minor fixes
    
    commit fedba5991962b22e7b4e67694135bbf8eeec62d7
    Author: Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
    Date:   Mon Apr 27 19:20:45 2026 +0900
    
        Add Resolution evaluation for AtlasQuadTreePoint
    
    commit a8c3dfcbe7349353193541e21bf590ebedbc8832
    Author: Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
    Date:   Mon Apr 27 17:27:02 2026 +0900
    
        Refactor: Base 2 helpers are now protected instead of anonymous
    
    commit 173ece82c2796a85b52e8249f3dd9b003fb1f100
    Author: Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
    Date:   Mon Apr 27 17:22:44 2026 +0900
    
        Refactor: AtlasQuadTree is now abstract
    
    commit 7818b3790f44db91f4040903cebf0dec33a1e1af
    Author: Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
    Date:   Mon Apr 27 17:14:45 2026 +0900
    
        Add AtlasQuadTreeBase cpp implementations
    
    commit 4c328fbf4db5a46299920f1323a42d9bcf48eafe
    Author: Hyungjun Kim <73098596+Rocketstein@users.noreply.github.com>
    Date:   Mon Apr 27 17:11:24 2026 +0900
    
        Add: Pointlight atlas quadtree base

--- KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.cpp
+++ KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.cpp
@@ -3,157 +3,17 @@
 
 #include <algorithm>
 
-namespace {
-	uint32 NextPowerOfTwo(uint32 v)
-	{
-		v--;
-		v |= v >> 1;
-		v |= v >> 2;
-		v |= v >> 4;
-		v |= v >> 8;
-		v |= v >> 16;
-		v++;
-		return v;
-	}
-
-	uint32 RoundToNearestPowerOfTwo(uint32 Value)
-	{
-		if (Value <= 1)
-			return 1;
-
-		uint32 Upper = NextPowerOfTwo(Value);
-		uint32 Lower = Upper >> 1;
-
-		return (Value - Lower < Upper - Value) ? Lower : Upper;
-	}
-}
-
 // Public functions
-void FShadowAtlasQuadTree::Init(float InAtlasSize, float InMinShadowMapResolution) {
-	if (InMinShadowMapResolution <= 0.f || InAtlasSize <= 0.f) {
-		return;
-	}
-	if (InMinShadowMapResolution > InAtlasSize) {
-		return;
-	}
-
-	AtlasSize = InAtlasSize;
-	RemainingSpace = AtlasSize * AtlasSize;
-	MinShadowMapResolution = InMinShadowMapResolution;
-	Node RootNode = {};
-	RootNode.TopLeft = FVector2(0.f, 0.f);
-	RootNode.Resolution = InAtlasSize;
-
-	Nodes.push_back(RootNode);
-}
 
 // Unused for now
-FAtlasRegion FShadowAtlasQuadTree::Add(const FSpotLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H) {
-	if (Nodes.empty()) return { 0, 0, 0, false };
-	float RequestedSize = EvaluateResolution(InLightInfo, CameraPos, Forward, FOV, H);
-	return AllocateNode(0, RequestedSize);
-}
+//FAtlasRegion FShadowAtlasQuadTree::Add(const FSpotLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H) {
+//	if (Nodes.empty()) return { 0, 0, 0, false };
+//	float RequestedSize = EvaluateResolution(InLightInfo, CameraPos, Forward, FOV, H);
+//	return AllocateNode(0, RequestedSize);
+//}
 
 void FShadowAtlasQuadTree::AddToBatch(const FSpotLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H) {
 	Batch.push_back({InLightInfo, EvaluateResolution(InLightInfo, CameraPos, Forward, FOV, H)});
-}
-
-void FShadowAtlasQuadTree::Reset() {
-	if (!Nodes.empty()) {
-		Nodes.resize(1);
-		Node& Root = Nodes[0];
-		Root.bOccupied = false;
-		Root.bSplit = false;
-		Root.Children[0] = Root.Children[1] = Root.Children[2] = Root.Children[3] = -1;
-	}
-
-	RemainingSpace = AtlasSize * AtlasSize;
-}
-
-void FShadowAtlasQuadTree::Clear() {
-	Nodes.clear();
-	RemainingSpace = AtlasSize * AtlasSize;
-}
-
-// Private helpers
-FAtlasRegion FShadowAtlasQuadTree::AllocateNode(int32 NodeIdx, uint32 RequestedSize) {
-	if (NodeIdx < 0
-		|| NodeIdx >= Nodes.size()
-		|| Nodes[NodeIdx].bOccupied
-		|| RequestedSize < (uint32) (MinShadowMapResolution)
-		|| !RemainingSpace) {
-		// Invalid Node index
-		return { 0, 0, 0, false };
-	}
-
-	if (RequestedSize * RequestedSize >= RemainingSpace && RequestedSize > MinShadowMapResolution)
-		return AllocateNode(NodeIdx, RequestedSize / 2);
-
-	Node node = Nodes[NodeIdx];
-	if (node.bSplit) {
-		for (int32 SubIdx : node.Children) {
-			if (Nodes[SubIdx].bOccupied || Nodes[SubIdx].Resolution < RequestedSize) continue;
-
-			// Greedily allocate the first child node that can fit the requested size
-			FAtlasRegion AllocatedRegion = AllocateNode(SubIdx, RequestedSize);
-			if (AllocatedRegion.bValid) {
-				return AllocatedRegion;
-			}
-		}
-
-		return { 0, 0, 0, false };
-	}
-	else {
-		if (node.Resolution == RequestedSize) {
-			RemainingSpace -= RequestedSize * RequestedSize;
-			Nodes[NodeIdx].bOccupied = true;
-			return { static_cast<uint32> (node.TopLeft.X), static_cast<uint32> (node.TopLeft.Y), static_cast<uint32>(node.Resolution), true };
-		}
-		else {
-
-			// Try again after splitting
-			if (Split(NodeIdx)) {
-				return AllocateNode(NodeIdx, RequestedSize);
-			}
-		}
-	}
-
-	return { 0, 0, 0, false };
-}
-
-bool FShadowAtlasQuadTree::Split(int32 Idx) {
-	if (Idx < 0 || Idx >= (int32)Nodes.size()) return false;
-
-	float HalfRes = Nodes[Idx].Resolution / 2.f;
-	if (HalfRes < MinShadowMapResolution) {
-		// Reached the minimum resolution. Should not split further.
-		return false;
-	}
-
-	FVector2 TL = Nodes[Idx].TopLeft;
-	for (int i = 0; i < 4; i++) {
-		Node NewNode = {};
-		NewNode.Resolution = HalfRes;
-
-		switch (i) {
-		case (0): // Top-Left
-			NewNode.TopLeft = TL;
-			break;
-		case (1): // Top-Right
-			NewNode.TopLeft = TL + FVector2(HalfRes, 0);
-			break;
-		case (2): // Bottom-Left
-			NewNode.TopLeft = TL + FVector2(0, HalfRes);
-			break;
-		case (3): // Bottom-Right
-			NewNode.TopLeft = TL + FVector2(HalfRes, HalfRes);
-			break;
-		}
-		Nodes.push_back(NewNode);
-		Nodes[Idx].Children[i] = (int32)(Nodes.size() - 1);
-	}
-	Nodes[Idx].bSplit = true;
-	return true;
 }
 
 float FShadowAtlasQuadTree::EvaluateResolution(const FSpotLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H) const {
@@ -173,7 +33,6 @@ float FShadowAtlasQuadTree::EvaluateResolution(const FSpotLightParams& InLightIn
 	}
 
 	auto z_view = (c_sphere - CameraPos).Dot(Forward);
-	float z_guard = 5.f;
 	z_view = z_view > z_guard ? z_view : z_guard;
 	auto r_ndc = (r_sphere / z_view) / tanf(FOV / 2.f);
 	auto r_pixel = r_ndc * H / 2.f;
--- KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.h
+++ KraftonEngine/Source/Engine/Render/Shadow/ShadowAtlasQuadTree.h
@@ -1,61 +1,22 @@
 ﻿#pragma once
-#include "Math/Vector.h"
-#include "Core/ClassTypes.h"
+#include "AtlasQuadTreeBase.h"
 
 // Exclusive to spotlights
 struct FSpotLightParams;
 
-struct Node {
-	// Dimensions
-	FVector2		TopLeft;
-	float			Resolution;
-
-	// Data
-	bool			bOccupied	= false;				// True if and only if this node is allocated to a shadow map by whole
-	bool			bSplit		= false;				// True if and only if this node has been split into a subtree
-	int32			Children[4] = { -1, -1, -1, -1 };	// indices into Nodes[], -1 = no child
-};
-
-struct FAtlasRegion { uint32 X, Y, Size; bool bValid; };
 
 // Buddy allocation quadtree for shadow atlas management
-class FShadowAtlasQuadTree {
+class FShadowAtlasQuadTree : public FAtlasQuadTreeBase {
 public:
-	// Initializes the head node of the atlas, and define the minimum resolution for the shadow maps to be allocated.
-	void Init(float InAtlasSize, float inMinShadowMapResolution);
-
-	// Try allocating the uv region for the input light source
-	FAtlasRegion Add(const FSpotLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H);
-
 	void AddToBatch(const FSpotLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H);
 
 	// Sorts the pending batch by evaluated resolution, then allocates all entries into the atlas.
-	TArray<FAtlasRegion> CommitBatch();
-
-	// Called every frame to reset the atlas.
-	void Reset();
-
-	// Hard clear including the root node
-	void Clear();
-
-	// Accessor
-	float GetAtlasSize() const { return AtlasSize; }
+	TArray<FAtlasRegion> CommitBatch() override;
 
 private:
-	// Allocates the node at NodeIdx and returns the corresponding atlas region. Returns invalid region if the node is occupied or too small.
-	FAtlasRegion AllocateNode(int32 NodeIdx, uint32 RequestedSize);
-
-	// Greedily splits the quadtree to find the best fit for the new shadow map
-	bool  Split(int32 Idx);
-
 	// Ranks the importance of the input light source based on its properties.
 	float EvaluateResolution(const FSpotLightParams& InLightInfo, FVector CameraPos, FVector Forward, float FOV, float H) const;
 
 private:
 	TArray<std::pair<FSpotLightParams, float>> Batch;
-	TArray<Node> Nodes;
-	float		 AtlasSize				= 4096.f;
-	float		 MinShadowMapResolution = 64.f;
-
-	float		 RemainingSpace			= 4096.f * 4096.f;
 };
