0482a8bdced2a9dd9b0133643d5f074f72973c4f
Park SangHyeok <tkdgurehf00@gmail.com>
2026-05-02 17:16:21 +0900

    Coroutine 실행 함수 호출 통일 self.* -> self:*

--- CrashEngine/Source/Engine/Component/ScriptComponent.cpp
+++ CrashEngine/Source/Engine/Component/ScriptComponent.cpp
@@ -303,10 +303,10 @@ void UScriptComponent::CallLuaTick(float DeltaTime)
 void UScriptComponent::BindFunctions()
 {
     BindFunction("start_coroutine",
-                 [this](sol::function Func) -> uint32
+                 [this](sol::table Self, sol::function Func) -> uint32
                  { return CoroutineExecutorSet.Start(Func); });
 
     BindFunction("stop_coroutine",
-                 [this](uint32 FuncKey) -> bool
+                 [this](sol::table Self, uint32 FuncKey) -> bool
                  { return CoroutineExecutorSet.Stop(FuncKey); });
 }
