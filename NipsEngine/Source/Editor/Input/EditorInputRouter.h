3f461210b4158dab94ceb801b5e875ef50e3d449
alogeclock <alogeclock@gmail.com>
2026-05-02 14:29:38 +0900

    Feat: Editor-Game 입력 분리를 위해 InputTypes 정의를 InputTypes.h에서 하도록 수정

--- NipsEngine/Source/Editor/Input/EditorInputRouter.h
+++ NipsEngine/Source/Editor/Input/EditorInputRouter.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Engine/Input/InputTypes.h"
 #include "Editor/Input/EditorWorldController.h"
 #include "Editor/Input/PIEController.h"
 
@@ -7,28 +8,6 @@ enum class EActiveEditorController
     EditorWorldController,
     PIEController,
     NilController,
-};
-
-enum class EKeyInputType
-{
-    KeyPressed,
-    KeyDown,
-    KeyReleased,
-    KeyNone,
-};
-
-enum class EMouseInputType
-{
-    E_MouseMoved,         // delta movement (DX, DY)
-    E_MouseMovedAbsolute, // viewport-local absolute position (X, Y)
-    E_LeftMouseClicked,   // LMB pressed down
-    E_LeftMouseDragged,   // LMB held + drag threshold met
-    E_LeftMouseDragEnded, // LMB drag released
-    E_LeftMouseButtonUp,  // LMB released (no drag / below threshold)
-    E_RightMouseClicked,
-    E_RightMouseDragged,
-    E_MiddleMouseDragged,
-    E_MouseWheelScrolled,
 };
 
 class FEditorInputRouter
