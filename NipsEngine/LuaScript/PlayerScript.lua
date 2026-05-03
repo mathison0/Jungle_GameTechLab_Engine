f5494891c89331e0ffd678d1fb43b55de5cb5e11
jskim-research <wltnwkd21@gmail.com>
2026-05-04 00:42:51 +0900

    Fix: dash don't use coroutine now

--- NipsEngine/LuaScript/PlayerScript.lua
+++ NipsEngine/LuaScript/PlayerScript.lua
@@ -1,8 +1,6 @@
 local Script = {}
 Script.__index = Script
 
--- Editor에 노출될 기본 Property 목록
---Type은 Int, Float, Bool, String, Vector이 있고 Default는 String
 Script.Properties = {
     bCanTick = {
         Type = "Bool",
@@ -20,16 +18,11 @@ end
 
 function Script:Dash(dir)
     self.bDoingDash = true
-
-    for i = 1, 10 do
-        self.owner.Location = self.owner.Location + dir * 0.75
-        coroutine.yield(WaitForSeconds(0.005))
-    end
-
-    self.bDoingDash = false
+    self.dashDir = dir
+    self.dashStepsLeft = 5
+    self.dashCooldown = 0.0
 end
 
--- instance를 반환하는 함수 Script 객체화하는 함수
 function Script.new(component, properties)
     local self = setmetatable({}, Script)
 
@@ -39,10 +32,11 @@ function Script.new(component, properties)
     self.PrevLocation = Vector(0, 0, 0)
     self.bDoingAttack = false
     self.bDoingDash = false
+    self.dashDir = Vector(0, 0, 0)
+    self.dashStepsLeft = 0
+    self.dashCooldown = 0.0
 
-    -- Editor에서 설정한 Property 값을 self에 복사
     properties = properties or {}
-
     for key, desc in pairs(Script.Properties) do
         if properties[key] ~= nil then
             self[key] = properties[key]
@@ -54,7 +48,6 @@ function Script.new(component, properties)
     return self
 end
 
---LifeCycle
 function Script:BeginPlay()
     Log("[BeginPlay] " .. tostring(self.owner.UUID))
 
@@ -62,18 +55,30 @@ function Script:BeginPlay()
         Log("Coroutine Start")
         coroutine.yield(WaitForSeconds(1.0))
         Log("1 seconds later")
-    end
-    )
+    end)
 end
 
 function Script:Tick(dt)
-    if not self.bCanTick then
+    if not self.bCanTick then return end
+
+    self.time = self.time + dt
+
+    -- Dash 스텝 처리
+    if self.bDoingDash then
+        self.dashCooldown = self.dashCooldown - dt
+        if self.dashCooldown <= 0 then
+            self.owner.Location = self.owner.Location + self.dashDir * 1.5
+            self.dashStepsLeft = self.dashStepsLeft - 1
+            self.dashCooldown = 0.01
+
+            if self.dashStepsLeft <= 0 then
+                self.bDoingDash = false
+            end
+        end
         return
     end
-    
-    self.time = self.time + dt
-    
-    local move = Vector(0,0,0)
+
+    local move = Vector(0, 0, 0)
 
     if Engine.API.Input.IsKeyDown("W") then
         move = move + self.owner:GetActorForwardVector()
@@ -101,36 +106,28 @@ function Script:Tick(dt)
     local delta = CurLoc - self.PrevLocation
     local speed = delta:Size() / math.max(dt, 1e-6)
 
-    local amplitude = 0.0001 + speed * 0.002   -- 크기
-    local freq = 6.0 + speed * 0.2             -- 속도
-
+    local amplitude = 0.0001 + speed * 0.002
+    local freq = 6.0 + speed * 0.2
     local offsetZ = math.cos(self.time * freq) * amplitude
     local offsetX = math.sin(self.time * freq * 0.5) * amplitude * 0.5
-    local camOffset = Vector(offsetX, 0, offsetZ)
-    Engine.API.World.AddViewTargetCameraLocation(camOffset)
+    Engine.API.World.AddViewTargetCameraLocation(Vector(offsetX, 0, offsetZ))
 
     self.owner.Location = CurLoc
-
     self.PrevLocation = CurLoc
-    
-    if not self.bDoingDash and Engine.API.Input.IsKeyPressed("Shift") then
-        StartCoroutine(function()
-            self:Dash(move:Normalized())
-        end)
-    end
-
-    -- Mouse
-    local sensivity = 0.003
 
     local rotation = self.owner.Rotation
     rotation.z = rotation.z + Engine.API.Input.GetMouseDelta().X * 0.1
     self.owner.Rotation = rotation
 
     Engine.API.World.GetViewTargetCamera():add_pitch_input(Engine.API.Input.GetMouseDelta().Y * 0.1)
-    
-    local player = Engine.API.World.GetPossessedActor()
 
+    local player = Engine.API.World.GetPossessedActor()
     self.camera_pitch = (self.camera_pitch or 0) + Engine.API.Input.GetMouseDelta().Y * 0.1
+
+    if not self.bDoingDash and Engine.API.Input.IsKeyPressed("Shift") then
+        local dir = move:Size() > 0.001 and move:Normalized() or self.owner:GetActorForwardVector()
+        self:Dash(dir)
+    end
 
     if not self.bDoingAttack and player and Engine.API.Input.IsMousePressed("LMB") then
         local slash = Engine.API.World.SpawnActor("ABladeSlash")
@@ -138,8 +135,8 @@ function Script:Tick(dt)
         local yaw = rotation.z
         local yaw_rad = math.rad(yaw)
         local degree = math.random(0, 180)
-        local pitch_x = degree * (math.sin(yaw_rad))
-        local pitch_y = degree * (math.cos(yaw_rad))
+        local pitch_x = degree * math.sin(yaw_rad)
+        local pitch_y = degree * math.cos(yaw_rad)
         local scale_long = 5
         local scale_short = 1
         local fx = math.abs(math.sin(yaw_rad))
@@ -149,12 +146,10 @@ function Script:Tick(dt)
 
         local pitch_rad = math.rad(self.camera_pitch)
         local fwd = self.owner:GetActorForwardVector()
-
-        -- forward를 pitch 각도만큼 위아래로 기울임
         local fwd_pitched = Vector(
             fwd.X * math.cos(pitch_rad),
             fwd.Y * math.cos(pitch_rad),
-            -math.sin(pitch_rad)  -- 위를 보면 올라가고 아래를 보면 내려감
+            -math.sin(pitch_rad)
         )
 
         slash.Rotation = Vector(pitch_y, pitch_x, yaw)
@@ -165,17 +160,14 @@ function Script:Tick(dt)
             self:Attack()
         end)
     end
-    
 end
 
 function Script:EndPlay()
     Log("[EndPlay] " .. tostring(self.owner.UUID))
 end
 
---Delegate
 function Script:OnHit(HitComponent, OtherActor, OtherComp, NormalImpulse, Hit)
     Log("[OnHit] " .. tostring(self.owner.UUID))
-
     if OtherActor ~= nil then
         Log("OtherActor: " .. tostring(OtherActor.Name))
     end
@@ -183,11 +175,9 @@ end
 
 function Script:OnBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult)
     Log("[OnBeginOverlap] " .. tostring(self.owner.UUID))
-
     if OtherActor ~= nil then
         Log("OtherActor: " .. tostring(OtherActor.Name))
     end
-
     if bFromSweep then
         Log("BeginOverlap from sweep")
     end
@@ -195,7 +185,6 @@ end
 
 function Script:OnEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult)
     Log("[OnEndOverlap] " .. tostring(self.owner.UUID))
-
     if OtherActor ~= nil then
         Log("OtherActor: " .. tostring(OtherActor.Name))
     end
