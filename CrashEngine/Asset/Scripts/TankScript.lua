a015227785f666a7e1d7c10c5acfdea21f006105
Park SangHyeok <tkdgurehf00@gmail.com>
2026-05-04 02:30:12 +0900

    기반 시스템 추가

--- CrashEngine/Asset/Scripts/TankScript.lua
+++ CrashEngine/Asset/Scripts/TankScript.lua
@@ -2,35 +2,49 @@
 local Script = {}
 local Query = require("Query")
 local Targeting = require("AI.TargetingAI")
-local Co = require("LuaCoroutine")
+local WeaponInventory = require("WeaponInventory")
+local LevelSystem = require("LevelSystem")
 
-function Script:FireMainGun()
-    while true do
-        self.FireHeadMainGun()
-        Co.Wait(0.5)
+function Script:BeginPlay()
+    Log("TankScript BeginPlay")
+
+    Log("SearchHeadMainGun")
+    self.HeadMainGun = self.GetComponentByName("UStaticMeshComponent", "HeadMainGun")
+    if self.HeadMainGun == nil or not self.HeadMainGun:IsValid() then
+        Log("Invalid HeadMainGun")
+    else
+        Log("StarCoroutineSearch")
+        self.Search = self.StartCoroutine(function()
+            Query.SearchActorClosestByTagCoroutine(self, self.HeadMainGun, "TestEnemy")
+        end)
+
+        Log("StarCoroutineTargeting")
+        self.Targeting = self.StartCoroutine(function()
+            Targeting.TargetingCoroutine(self, self.HeadMainGun, true)
+        end)
+    end
+
+    self.WeaponInventory = WeaponInventory.New(self)
+    self.LevelSystem = LevelSystem.New(self, self.WeaponInventory)
+
+    self.WeaponInventory:AddWeapon("MainCannon")
+
+    -- Test only. In actual gameplay, enemy kills should call AddExp.
+    self.LevelSystem:AddExp(30)
+end
+
+function Script:AddExp(amount)
+    if self.LevelSystem ~= nil then
+        self.LevelSystem:AddExp(amount)
     end
 end
 
-function Script:BeginPlay()
-    Log("SearchHeadMainGun")
-    self.HeadMainGun = self.GetComponentByName("UStaticMeshComponent", "HeadMainGun");
-    if self.HeadMainGun == nil or not self.HeadMainGun:IsValid() then
-        Log("Invalid HeadMainGun")
-        return
+function Script:OpenChest()
+    if self.WeaponInventory ~= nil then
+        return self.WeaponInventory:OpenChest()
     end
 
-    Log("StarCoroutineSearch")
-    self.Search = self.StartCoroutine(function()
-        Query.SearchActorClosestByTagCoroutine(self, self.HeadMainGun, "TestEnemy")
-    end)
-    Log("StarCoroutineTargeting")
-    self.Targeting = self.StartCoroutine(function()
-        Targeting.TargetingCoroutine(self, self.HeadMainGun, true)
-    end)
-    Log("StarCoroutineHeadMainGunFire")
-    self.HeadMainGunFire = self.StartCoroutine(function()
-        Script.FireMainGun(self)
-    end)
+    return false
 end
 
 function Script:Tick(deltaTime)
@@ -38,6 +52,14 @@ function Script:Tick(deltaTime)
 end
 
 function Script:EndPlay()
+    if self.WeaponInventory ~= nil then
+        for _, weapon in pairs(self.WeaponInventory.Weapons) do
+            if weapon.Stop ~= nil then
+                weapon:Stop()
+            end
+        end
+    end
+
     self.StopAllCoroutines()
 end
 
---
+++ CrashEngine/Asset/Scripts/Weapons/MainCannonWeapon.lua
@@ -0,0 +1,88 @@
+local Co = require("LuaCoroutine")
+local WeaponDefs = require("WeaponDefs")
+
+local MainCannonWeapon = {}
+MainCannonWeapon.__index = MainCannonWeapon
+
+function MainCannonWeapon.New(owner)
+    local self = setmetatable({}, MainCannonWeapon)
+    self.Owner = owner
+    self.Id = "MainCannon"
+    self.Level = 1
+    self.Data = WeaponDefs.MainCannon.Levels[self.Level]
+    self.Coroutine = nil
+    self.IsRunning = false
+    return self
+end
+
+function MainCannonWeapon:Start()
+    if self.IsRunning then
+        return
+    end
+
+    self.IsRunning = true
+    Log("MainCannon FireLoop started")
+
+    if self.Owner ~= nil and self.Owner.StartCoroutine ~= nil then
+        self.Coroutine = self.Owner.StartCoroutine(function()
+            self:FireLoop()
+        end)
+    else
+        Log("StartCoroutine is nil")
+    end
+end
+
+function MainCannonWeapon:Stop()
+    self.IsRunning = false
+
+    if self.Owner ~= nil and self.Owner.StopCoroutine ~= nil and self.Coroutine ~= nil then
+        self.Owner.StopCoroutine(self.Coroutine)
+    end
+
+    self.Coroutine = nil
+end
+
+function MainCannonWeapon:FireLoop()
+    while self.IsRunning do
+        if self.Owner ~= nil and self.Owner.FireHeadMainGun ~= nil then
+            self.Owner.FireHeadMainGun()
+        else
+            Log("FireHeadMainGun is nil")
+        end
+
+        Co.Wait(self.Data.FireInterval)
+    end
+end
+
+function MainCannonWeapon:Upgrade()
+    if self.Level >= WeaponDefs.MainCannon.MaxNormalLevel then
+        Log("MainCannon is max normal level")
+        return false
+    end
+
+    self.Level = self.Level + 1
+    self.Data = WeaponDefs.MainCannon.Levels[self.Level]
+    Log("MainCannon upgraded. level = " .. tostring(self.Level) .. ", interval = " .. tostring(self.Data.FireInterval))
+    return true
+end
+
+function MainCannonWeapon:IsMaxLevel()
+    return self.Level >= WeaponDefs.MainCannon.MaxNormalLevel
+end
+
+function MainCannonWeapon:CanEvolve()
+    return self.Level == WeaponDefs.MainCannon.MaxNormalLevel
+end
+
+function MainCannonWeapon:Evolve()
+    if not self:CanEvolve() then
+        return false
+    end
+
+    self.Level = 3
+    self.Data = WeaponDefs.MainCannon.Levels[self.Level]
+    Log("MainCannon evolved. level = " .. tostring(self.Level) .. ", interval = " .. tostring(self.Data.FireInterval))
+    return true
+end
+
+return MainCannonWeapon
