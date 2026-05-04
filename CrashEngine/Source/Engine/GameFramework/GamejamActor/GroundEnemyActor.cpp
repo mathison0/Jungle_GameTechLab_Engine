78c4b7fd3e82d6d8ea8bcd93232780455c31f09d
김태현 <th2252@gmail.com>
2026-05-04 15:57:42 +0900

    Add GroundEnemyActor
    병사 모델링 적용하고 충돌 반지름 1->0.5로 줄인 버전

---
+++ CrashEngine/Source/Engine/GameFramework/GamejamActor/GroundEnemyActor.cpp
@@ -0,0 +1,28 @@
+﻿#include "GroundEnemyActor.h"
+
+#include "Component/StaticMeshComponent.h"
+#include "Component/Collision/CircleCollider2DComponent.h"
+#include "Mesh/ObjManager.h"
+#include "Runtime/Engine.h"
+
+IMPLEMENT_CLASS(AGroundEnemyActor, AEnemyBaseActor)
+
+UStaticMeshComponent* AGroundEnemyActor::GetDefaultMeshComponent()
+{
+    ID3D11Device* Device = GEngine->GetRenderer().GetFD3DDevice().GetDevice();
+    UStaticMesh* Asset = FObjManager::LoadObjStaticMesh(FPaths::ContentRelativePath("Models/ArmyMan/ArmyMan-posed.obj"), Device);
+    
+    UStaticMeshComponent* MeshComp = AddComponent<UStaticMeshComponent>();
+    MeshComp->SetStaticMesh(Asset);
+    MeshComp->SetRelativeScale(FVector(2.0f));
+    return MeshComp;
+}
+
+UCollider2DComponent* AGroundEnemyActor::GetDefaultColliderComponent()
+{
+    UCircleCollider2DComponent* Collider = AddComponent<UCircleCollider2DComponent>();
+    Collider->SetCollisionChannel(ECollisionChannel::Enemy);
+    Collider->SetGenerateOverlapEvents(true);
+    Collider->SetRadius(0.5f);
+    return Collider;
+}
