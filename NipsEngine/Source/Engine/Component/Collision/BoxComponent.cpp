a9eefee6a3964846ebe4a2f49d3be8c7bed9e361
Kihoon Kim <leonardkim1209@gmail.com>
2026-05-04 09:18:39 +0900

    fix: now collision test is based on OBB, not AABB

--- NipsEngine/Source/Engine/Component/Collision/BoxComponent.cpp
+++ NipsEngine/Source/Engine/Component/Collision/BoxComponent.cpp
@@ -2,6 +2,12 @@
 
 #include "Core/PropertyTypes.h"
 #include "Engine/Serialization/Archive.h"
+#include "Math/Quat.h"
+#include "Math/Utils.h"
+
+#include <algorithm>
+#include <cmath>
+#include <limits>
 
 DEFINE_CLASS(UBoxComponent, UShapeComponent)
 REGISTER_FACTORY(UBoxComponent)
@@ -38,11 +44,52 @@ void UBoxComponent::UpdateWorldAABB() const
     WorldAABB = FAABB::TransformAABB(LocalAABB, ShapeWorldMatrix);
 }
 
-// RayCasting 전용 AABB
+// RayCasting 전용 OBB
 bool UBoxComponent::RaycastMesh(const FRay& Ray, FHitResult& OutHitResult)
 {
-    float HitT = 0.0f;
-    if (!GetWorldAABB().IntersectRay(Ray, HitT))
+    const FVector SafeExtent(std::fabs(BoxExtent.X), std::fabs(BoxExtent.Y), std::fabs(BoxExtent.Z));
+    const FQuat WorldRotation(GetWorldMatrix().GetRotationMatrix());
+    const FQuat InverseRotation = WorldRotation.GetNormalized().Inverse();
+    const FVector LocalOrigin = InverseRotation * (Ray.Origin - GetWorldLocation());
+    const FVector LocalDirection = InverseRotation * Ray.Direction;
+
+    float TMin = 0.0f;
+    float TMax = std::numeric_limits<float>::max();
+
+    for (int32 Axis = 0; Axis < 3; ++Axis)
+    {
+        const float Origin = LocalOrigin[Axis];
+        const float Direction = LocalDirection[Axis];
+        const float Min = -SafeExtent[Axis];
+        const float Max = SafeExtent[Axis];
+
+        if (std::fabs(Direction) < MathUtil::Epsilon)
+        {
+            if (Origin < Min || Origin > Max)
+            {
+                return false;
+            }
+            continue;
+        }
+
+        const float InvDirection = 1.0f / Direction;
+        float T1 = (Min - Origin) * InvDirection;
+        float T2 = (Max - Origin) * InvDirection;
+        if (T1 > T2)
+        {
+            std::swap(T1, T2);
+        }
+
+        TMin = std::max(TMin, T1);
+        TMax = std::min(TMax, T2);
+        if (TMin > TMax)
+        {
+            return false;
+        }
+    }
+
+    const float HitT = TMin >= 0.0f ? TMin : TMax;
+    if (HitT < 0.0f)
     {
         return false;
     }
