5ea0a58f4a8cfb88b13d224cf731912336f8a0ac
Park SangHyeok <tkdgurehf00@gmail.com>
2026-05-04 15:39:10 +0900

    feat : Targeting 기능 추가

--- CrashEngine/Asset/Scripts/Weapons/MainCannonWeapon.lua
+++ CrashEngine/Asset/Scripts/Weapons/MainCannonWeapon.lua
@@ -4,14 +4,29 @@ local WeaponDefs = require("WeaponDefs")
 local MainCannonWeapon = {}
 MainCannonWeapon.__index = MainCannonWeapon
 
+local TARGET_TAG = "Enemy"
+local DEFAULT_RANGE = 10000.0
+local DEFAULT_SEARCH_INTERVAL = 0.2
+
 function MainCannonWeapon.New(owner)
     local self = setmetatable({}, MainCannonWeapon)
+
     self.Owner = owner
     self.Id = "MainCannon"
     self.Level = 1
     self.Data = WeaponDefs.MainCannon.Levels[self.Level]
-    self.Coroutine = nil
+
     self.IsRunning = false
+
+    self.Target = nil
+
+    self.VisualComponent = nil
+    self.MuzzleComponent = nil
+
+    self.SearchCoroutine = nil
+    self.TargetingCoroutine = nil
+    self.FireCoroutine = nil
+
     return self
 end
 
@@ -21,33 +36,169 @@ function MainCannonWeapon:Start()
     end
 
     self.IsRunning = true
-    Log("MainCannon FireLoop started")
+    self:RefreshComponents()
 
-    if self.Owner ~= nil and self.Owner.StartCoroutine ~= nil then
-        self.Coroutine = self.Owner.StartCoroutine(function()
-            self:FireLoop()
-        end)
-    else
-        Log("StartCoroutine is nil")
+    Log("[MainCannon] Start")
+
+    if self.Owner == nil or self.Owner.StartCoroutine == nil then
+        Log("[MainCannon] StartCoroutine is nil")
+        return
     end
+
+    self.SearchCoroutine = self.Owner.StartCoroutine(function()
+        self:SearchLoop()
+    end)
+
+    self.TargetingCoroutine = self.Owner.StartCoroutine(function()
+        self:TargetingLoop()
+    end)
+
+    self.FireCoroutine = self.Owner.StartCoroutine(function()
+        self:FireLoop()
+    end)
 end
 
 function MainCannonWeapon:Stop()
     self.IsRunning = false
 
-    if self.Owner ~= nil and self.Owner.StopCoroutine ~= nil and self.Coroutine ~= nil then
-        self.Owner.StopCoroutine(self.Coroutine)
+    if self.Owner ~= nil and self.Owner.StopCoroutine ~= nil then
+        if self.SearchCoroutine ~= nil then
+            self.Owner.StopCoroutine(self.SearchCoroutine)
+        end
+
+        if self.TargetingCoroutine ~= nil then
+            self.Owner.StopCoroutine(self.TargetingCoroutine)
+        end
+
+        if self.FireCoroutine ~= nil then
+            self.Owner.StopCoroutine(self.FireCoroutine)
+        end
     end
 
-    self.Coroutine = nil
+    self.SearchCoroutine = nil
+    self.TargetingCoroutine = nil
+    self.FireCoroutine = nil
+
+    self.Target = nil
+end
+
+function MainCannonWeapon:RefreshComponents()
+    if self.Owner == nil or self.Owner.GetComponentByName == nil then
+        return
+    end
+
+    self.VisualComponent = self.Owner.GetComponentByName(
+        "UStaticMeshComponent",
+        "Visual_MainCannon_0"
+    )
+
+    self.MuzzleComponent = self.Owner.GetComponentByName(
+        "USceneComponent",
+        "Muzzle_MainCannon_0"
+    )
+
+    if self.VisualComponent == nil or not self.VisualComponent:IsValid() then
+        Log("[MainCannon] Visual_MainCannon_0 not found")
+    end
+
+    if self.MuzzleComponent == nil or not self.MuzzleComponent:IsValid() then
+        Log("[MainCannon] Muzzle_MainCannon_0 not found")
+    end
+end
+
+function MainCannonWeapon:GetSearchOrigin()
+    if self.MuzzleComponent ~= nil and self.MuzzleComponent:IsValid() then
+        return self.MuzzleComponent:GetWorldLocation()
+    end
+
+    if self.VisualComponent ~= nil and self.VisualComponent:IsValid() then
+        return self.VisualComponent:GetWorldLocation()
+    end
+
+    local actor = nil
+    if self.Owner ~= nil and self.Owner.GetActor ~= nil then
+        actor = self.Owner.GetActor()
+    end
+
+    if actor ~= nil and actor:IsValid() then
+        return actor:GetLocation()
+    end
+
+    return nil
+end
+
+function MainCannonWeapon:IsTargetValid()
+    return self.Target ~= nil and self.Target:IsValid()
+end
+
+function MainCannonWeapon:SearchTarget()
+    if self.Owner == nil or self.Owner.QueryActorByTagClosest == nil then
+        return
+    end
+
+    local origin = self:GetSearchOrigin()
+    if origin == nil then
+        self:RefreshComponents()
+        origin = self:GetSearchOrigin()
+    end
+
+    if origin == nil then
+        return
+    end
+
+    local range = self.Data.Range or self.Data.TargetSearchRadius or DEFAULT_RANGE
+
+    self.Target = self.Owner.QueryActorByTagClosest(
+        TARGET_TAG,
+        origin,
+        range
+    )
+end
+
+function MainCannonWeapon:SearchLoop()
+    while self.IsRunning do
+        self:SearchTarget()
+
+        local interval =
+            self.Data.TargetRefreshInterval or
+            self.Data.SearchInterval or
+            DEFAULT_SEARCH_INTERVAL
+
+        Co.Wait(interval)
+    end
+end
+
+function MainCannonWeapon:TargetingLoop()
+    while self.IsRunning do
+        Co.WaitNextFrame()
+
+        if self.VisualComponent == nil or not self.VisualComponent:IsValid() then
+            self:RefreshComponents()
+        end
+
+        if self.VisualComponent ~= nil and self.VisualComponent:IsValid() and self:IsTargetValid() then
+            local myPos = self.VisualComponent:GetWorldLocation()
+            local targetPos = self.Target:GetLocation()
+
+            local z = myPos.z or myPos[3]
+            if z ~= nil then
+                targetPos[3] = z
+                targetPos.z = z
+            end
+
+            self.VisualComponent:LookAt(targetPos)
+        end
+    end
 end
 
 function MainCannonWeapon:FireLoop()
     while self.IsRunning do
-        if self.Owner ~= nil and self.Owner.FireLinearProjectile ~= nil then
-            self.Owner.FireLinearProjectile("MainCannon", self.Data, 0)
-        else
-            Log("[MainCannon] FireLinearProjectile is nil")
+        if self:IsTargetValid() then
+            if self.Owner ~= nil and self.Owner.FireLinearProjectile ~= nil then
+                self.Owner.FireLinearProjectile("MainCannon", self.Data, 0)
+            else
+                Log("[MainCannon] FireLinearProjectile is nil")
+            end
         end
 
         Co.Wait(self.Data.FireInterval)
@@ -62,7 +213,18 @@ function MainCannonWeapon:Upgrade()
 
     self.Level = self.Level + 1
     self.Data = WeaponDefs.MainCannon.Levels[self.Level]
-    Log("MainCannon upgraded. level = " .. tostring(self.Level) .. ", interval = " .. tostring(self.Data.FireInterval))
+
+    -- WeaponInventory가 visual layout을 갱신한 뒤에도 이름은 같지만,
+    -- 컴포넌트 재생성/숨김 가능성 때문에 다음 루프에서 다시 찾게 한다.
+    self:RefreshComponents()
+
+    Log(
+        "MainCannon upgraded. level = " ..
+        tostring(self.Level) ..
+        ", interval = " ..
+        tostring(self.Data.FireInterval)
+    )
+
     return true
 end
 
@@ -81,7 +243,16 @@ function MainCannonWeapon:Evolve()
 
     self.Level = 3
     self.Data = WeaponDefs.MainCannon.Levels[self.Level]
-    Log("MainCannon evolved. level = " .. tostring(self.Level) .. ", interval = " .. tostring(self.Data.FireInterval))
+
+    self:RefreshComponents()
+
+    Log(
+        "MainCannon evolved. level = " ..
+        tostring(self.Level) ..
+        ", interval = " ..
+        tostring(self.Data.FireInterval)
+    )
+
     return true
 end
 
