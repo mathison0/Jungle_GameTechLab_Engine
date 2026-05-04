1c4d45ffb85c7dcc791e6a1548000d658a6b7d73
김태현 <th2252@gmail.com>
2026-05-04 20:55:51 +0900

    Fix: Lua Session Error

--- CrashEngine/Asset/Scripts/GameManagerComponent.lua
+++ CrashEngine/Asset/Scripts/GameManagerComponent.lua
@@ -1,56 +1,39 @@
 --[[
-
-Property 노출 등을 위한 GameManager의 인터페이스용 인스턴스
-
+    GameManagerComponent.lua
+    GameManager의 세션 설정을 관리하고 메인 틱을 구동합니다.
 ]]
 
----@class GameManagerComponent : ScriptComponent
 local Script = {
     properties = {
         GameTimeLimit = { type = "float", default = 600.0, min = 10.0, max = 3600.0, speed = 10.0 },
     }
 }
 
-local GameManager = require("GameManager")
-
 function Script:BeginPlay()
-    Log("GameManagerComponent: BeginPlay")
+    Log("--- GameManagerComponent: BeginPlay ---")
     
-    -- 플레이어 찾기
-    local world = self:GetWorld()
-    if not world or not world:IsValid() then
-        Log("GameManagerComponent: Invalid World")
-        return
-    end
-
-    -- 플레이어 태그를 가진 액터 탐색
-    local playerActor = self:QueryActorByTagClosest("Player", {x=0, y=0, z=0}, 1000000.0)
-    if not playerActor or not playerActor:IsValid() then
-        Log("GameManagerComponent: Player not found!")
-        -- 플레이어가 아직 생성되지 않았을 수 있으므로, 
-        -- 실제 프로젝트에서는 PlayerController 등에서 역으로 등록하거나 
-        -- 약간의 지연 후 다시 찾을 필요가 있을 수 있음.
-        return
-    end
-
-    -- 플레이어의 스크립트 컴포넌트 가져오기 (UScriptComponent)
-    local playerScript = playerActor:GetComponent("UScriptComponent")
-    if not playerScript then
-        Log("GameManagerComponent: Player script (UScriptComponent) not found!")
-        return
-    end
-
-    -- GameManager 초기화 및 시작
-    GameManager.Init(playerScript, self.GameTimeLimit)
-    GameManager.OnGameStart()
+    local GameManager = require("GameManager")
+    -- 플레이어 등록 여부와 상관없이 세션 설정(시간 제한 등)을 먼저 준비합니다.
+    GameManager.PrepareSession(self.GameTimeLimit)
 end
 
 function Script:Tick(deltaTime)
+    local GameManager = require("GameManager")
+    -- GameManager가 플레이어 등록까지 완료된 후에만 실제 로직(시간 감소 등)을 수행합니다.
     GameManager.Tick(deltaTime)
 end
 
 function Script:EndPlay()
     Log("GameManagerComponent: EndPlay")
+    
+    local GameManager = require("GameManager")
+    if GameManager.WeaponInventory ~= nil then
+        for _, weapon in pairs(GameManager.WeaponInventory.Weapons) do
+            if weapon.Stop ~= nil then
+                weapon:Stop()
+            end
+        end
+    end
 end
 
 return Script
