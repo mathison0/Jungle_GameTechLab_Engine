ae64d4625f4082b7727edc54e7f6d8c8cbaa64ee
alogeclock <alogeclock@gmail.com>
2026-05-06 13:42:52 +0900

    Feat: FCameraOverlayInfo 일단은 FPostProcessSettings로 변경

--- NipsEngine/Source/Engine/Camera/CameraModifier/FadeCameraModifier.h
+++ NipsEngine/Source/Engine/Camera/CameraModifier/FadeCameraModifier.h
@@ -1,1 +1,34 @@
-#pragma once
+﻿#pragma once
+
+#include "Engine/Camera/CameraModifier.h"
+#include "Engine/Camera/PlayerCameraManager.h"
+#include "Math/Color.h"
+
+class UFadeCameraModifier : public UCameraModifier
+{
+public:
+	DECLARE_CLASS(UFadeCameraModifier, UCameraModifier)
+
+	void StartFade(const FColor& InColor, float FromAlpha, float ToAlpha, float Duration);
+	void SetFade(const FColor& InColor, float Alpha);
+	void ClearFade();
+
+	bool ModifyOverlay(float DeltaTime, FCameraOverlayInfo& InOutOverlay) override;
+
+	bool IsFading() const { return bFading; }
+	float GetFadeAlpha() const { return CurrentAlpha; }
+	const FColor& GetFadeColor() const { return FadeColor; }
+
+
+private:
+	FColor FadeColor = FColor::Black();
+
+	float FromAlpha = 0.0f;
+	float ToAlpha = 0.0f;
+	float CurrentAlpha = 0.0f;
+
+	float FadeTime = 0.0f;
+	float FadeRemainingTime = 0.0f;
+
+	bool bFading = false;
+};
