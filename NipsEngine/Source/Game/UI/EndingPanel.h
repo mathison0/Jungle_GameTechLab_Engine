fa199850a473028934ecaf0b4edb2a0c000de849
alogeclock <alogeclock@gmail.com>
2026-05-06 21:14:17 +0900

    Feat: Credits Cut Scene in Ending Scene

--- NipsEngine/Source/Game/UI/EndingPanel.h
+++ NipsEngine/Source/Game/UI/EndingPanel.h
@@ -1,22 +1,39 @@
-#pragma once
+﻿#pragma once
 
 #include "Game/UI/GameUISystem.h"
 
 class EndingPanel
 {
 public:
-    static void Render(EUIRenderMode Mode);
-    static void Tick(float DeltaTime);
-    static void Reset();
-    static bool ShouldShowTheEnd();
-    static float GetFadeAlpha();
-    static const char* GetImagePath();
+	static void Render(EUIRenderMode Mode);
+	static void Tick(float DeltaTime);
+	static void Reset();
+	static bool ShouldShowTheEnd();
+	static bool ShouldShowCredits();
+	static bool ShouldShowBlackFade();
+	static bool ShouldShowEndingVisual();
+	static float GetFadeAlpha();
+	static float GetCreditAlpha();
+	static float GetBlackFadeAlpha();
+	static const char* GetImagePath();
 
 private:
-    static void StartDialogue();
+	static void StartDialogue();
+	static void StartFinalScene();
 
-    static float FadeTimer;
-    static bool  bShowTheEnd;
-    static bool  bExitCalled;
-    static EEndingType ActiveEndingType;
+	enum class EEndingSequencePhase
+	{
+		Dialogue,
+		FadeToBlack,
+		Credits,
+		CreditFadeOut,
+		Final
+	};
+
+	static float FadeTimer;
+	static float SequenceTimer;
+	static bool  bShowTheEnd;
+	static bool  bExitCalled;
+	static EEndingSequencePhase SequencePhase;
+	static EEndingType ActiveEndingType;
 };
