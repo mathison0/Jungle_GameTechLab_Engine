6f7d7fb3abe59323d56c927e07db152ffc0961db
남윤지 <rora000924@gmail.com>
2026-05-06 19:11:12 +0900

    Feat: Add Curve UI

---
+++ NipsEngine/Source/Engine/Camera/Modifier/CameraShakeModifier.cpp
@@ -0,0 +1,39 @@
+﻿#include "CameraShakeModifier.h"
+#include "Engine/Camera/PlayerCameraManager.h"
+#include "Engine/Math/Utils.h"
+#include <cmath>
+
+DEFINE_CLASS(UCameraShakeModifier, UCameraModifier)
+
+void UCameraShakeModifier::StartShake(float InAmplitude, float InFrequency, float InDuration)
+{
+    Amplitude = InAmplitude;
+    Frequency = InFrequency;
+    Duration = InDuration;
+    Elapsed = 0.0f;
+    bShaking = true;
+    EnableModifier();
+}
+
+void UCameraShakeModifier::StopShake()
+{
+    bShaking = false;
+}
+
+bool UCameraShakeModifier::ModifyCamera(float DeltaTime, FCameraViewInfo& InOutView)
+{
+    if (!bShaking) return false;
+
+	Elapsed += DeltaTime;
+    if (Elapsed >= Duration)
+    {
+        bShaking = false;
+        return false;
+	}
+
+	const float t = Elapsed / Duration;
+
+
+
+    return false;
+}
