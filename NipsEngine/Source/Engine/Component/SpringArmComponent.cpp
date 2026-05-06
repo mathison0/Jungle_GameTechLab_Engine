79c552395593fdc33e01d84ad6cfc708e99b4e7e
남윤지 <rora000924@gmail.com>
2026-05-06 11:32:19 +0900

    Add SpringArm Property

--- NipsEngine/Source/Engine/Component/SpringArmComponent.cpp
+++ NipsEngine/Source/Engine/Component/SpringArmComponent.cpp
@@ -3,8 +3,25 @@
 #include "ActorComponent.h"
 #include "GameFrameWork/World.h"
 
+DEFINE_CLASS(USpringArmComponent, USceneComponent)
+
 void USpringArmComponent::TickComponent(float DeltaTime)
 {
+    if (bEnableCameraRotationLag)
+    {
+        FVector RelativeRotationEuler = GetRelativeRotation();
+        FQuat TargetRot = FQuat::MakeFromEuler(RelativeRotationEuler);
+
+        if (CurrentArmRotation.IsIdentity())
+        {
+            CurrentArmRotation = TargetRot;
+        }
+
+        CurrentArmRotation = FQuat::Slerp(CurrentArmRotation, TargetRot, MathUtil::Clamp(DeltaTime * CameraRotationLagSpeed, 0.f, 1.f));
+
+        SetRelativeRotation(CurrentArmRotation.Euler());
+    }
+
     FVector ParentWorldPos = GetParent() ? GetParent()->GetWorldLocation() : FVector::ZeroVector;
 	FVector ForwardVector = GetForwardVector();
     FVector DesiredPos = ParentWorldPos - (ForwardVector * TargetArmLength) + SocketOffset;
@@ -20,7 +37,6 @@ void USpringArmComponent::TickComponent(float DeltaTime)
 
         if (World->LineTraceSingle(Ray, TargetArmLength, Hit, GetOwner()))
         {
-            float ProbeRadius = 12.0f;
             DesiredPos = Hit.Location + (ForwardVector * ProbeRadius);
         }
 	}
@@ -38,4 +54,43 @@ void USpringArmComponent::TickComponent(float DeltaTime)
 	}
 
 	SetWorldLocation(CurrentArmEndpoint);
+}
+
+void USpringArmComponent::GetEditableProperties(TArray<FPropertyDescriptor>& OutProps)
+{
+    USceneComponent::GetEditableProperties(OutProps);
+
+    // 카메라 기본 설정
+    OutProps.push_back({ "Target Arm Length", EPropertyType::Float, &TargetArmLength });
+    OutProps.push_back({ "Socket Offset", EPropertyType::Vec3, &SocketOffset });
+
+    // 충돌 설정
+    OutProps.push_back({ "Do Collision Test", EPropertyType::Bool, &bDoCollisionTest });
+    OutProps.push_back({ "Probe Radius", EPropertyType::Float, &ProbeRadius });
+
+    // 지연(Lag) 설정
+    OutProps.push_back({ "Enable Location Lag", EPropertyType::Bool, &bEnableCameraLag });
+    OutProps.push_back({ "Location Lag Speed", EPropertyType::Float, &CameraLagSpeed });
+
+    OutProps.push_back({ "Enable Rotation Lag", EPropertyType::Bool, &bEnableCameraRotationLag });
+    OutProps.push_back({ "Rotation Lag Speed", EPropertyType::Float, &CameraRotationLagSpeed });
+}
+
+void USpringArmComponent::PostEditProperty(const char* PropertyName)
+{
+    USceneComponent::PostEditProperty(PropertyName);
+
+    TargetArmLength = std::max(0.0f, TargetArmLength);
+    CameraLagSpeed = std::max(0.0f, CameraLagSpeed);
+    CameraRotationLagSpeed = std::max(0.0f, CameraRotationLagSpeed);
+    ProbeRadius = std::max(0.0f, ProbeRadius);
+
+    if (strcmp(PropertyName, "Target Arm Length") == 0 ||
+        strcmp(PropertyName, "Enable Location Lag") == 0)
+    {
+        FVector ParentWorldPos = GetParent() ? GetParent()->GetWorldLocation() : FVector::ZeroVector;
+        FVector DesiredPos = ParentWorldPos - (GetForwardVector() * TargetArmLength) + SocketOffset;
+
+        CurrentArmEndpoint = DesiredPos;
+    }
 }
