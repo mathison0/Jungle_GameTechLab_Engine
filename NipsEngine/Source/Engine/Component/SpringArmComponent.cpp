2dc512ab278e147117108234c5de74c35d2a68e7
남윤지 <rora000924@gmail.com>
2026-05-06 11:09:24 +0900

    Feat: SpringArmComponent

---
+++ NipsEngine/Source/Engine/Component/SpringArmComponent.cpp
@@ -0,0 +1,41 @@
+﻿#include "SpringArmComponent.h"
+#include "Core/CollisionTypes.h"
+#include "ActorComponent.h"
+#include "GameFrameWork/World.h"
+
+void USpringArmComponent::TickComponent(float DeltaTime)
+{
+    FVector ParentWorldPos = GetParent() ? GetParent()->GetWorldLocation() : FVector::ZeroVector;
+	FVector ForwardVector = GetForwardVector();
+    FVector DesiredPos = ParentWorldPos - (ForwardVector * TargetArmLength) + SocketOffset;
+
+	UWorld* World = GetOwner() ? GetOwner()->GetFocusedWorld() : nullptr;
+    if (!World)
+        return;
+
+	if (bDoCollisionTest)
+	{
+        FHitResult Hit;
+        FRay Ray(ParentWorldPos, -ForwardVector);
+
+        if (World->LineTraceSingle(Ray, TargetArmLength, Hit, GetOwner()))
+        {
+            float ProbeRadius = 12.0f;
+            DesiredPos = Hit.Location + (ForwardVector * ProbeRadius);
+        }
+	}
+
+	if (bEnableCameraLag)
+	{
+        if (CurrentArmEndpoint.IsZero())
+			CurrentArmEndpoint = DesiredPos;
+
+		CurrentArmEndpoint = FVector::Lerp(CurrentArmEndpoint, DesiredPos, MathUtil::Clamp(DeltaTime * CameraLagSpeed, 0.f, 1.f));
+	}
+	else
+	{
+        CurrentArmEndpoint = DesiredPos;
+	}
+
+	SetWorldLocation(CurrentArmEndpoint);
+}
---
+++ NipsEngine/Source/Engine/Component/SpringArmComponent.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "SceneComponent.h"
+
+class USpringArmComponent : public USceneComponent
+{
+public:
+    float TargetArmLength = 3000.f;
+    FVector SocketOffset = {};
+    float CameraLagSpeed = 10.f;
+    bool bEnableCameraLag = true;
+    bool bDoCollisionTest = true;
+
+	void TickComponent(float DeltaTime) override;
+
+private:
+    FVector CurrentArmEndpoint = {};
+};
