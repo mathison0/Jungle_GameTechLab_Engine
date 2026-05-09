25010a5274e2169283eb1a4153bb767d33ce9485
김태현 <th2252@gmail.com>
2026-05-09 23:19:21 +0900

    fix: Build Error
    Skeletal Mesh의 Render Buffer 생성하는 부분이 AI가 작성해준 코드가 있었는데 Mesh 리팩토링하면서 Merge conflict 발생, 주석 처리해두었음
    재구현 필요.

--- CrashEngine/Source/Engine/Mesh/SkeletalMesh.cpp
+++ CrashEngine/Source/Engine/Mesh/SkeletalMesh.cpp
@@ -171,6 +171,9 @@ void USkeletalSubMesh::InitResources(ID3D11Device* InDevice)
     RenderMeshData.Vertices = SkeletalSubMeshAsset->Vertices;
     RenderMeshData.Indices = SkeletalSubMeshAsset->Indices;
 
-    SkeletalSubMeshAsset->RenderBuffer = std::make_unique<FMeshBuffer>();
-    SkeletalSubMeshAsset->RenderBuffer->Create(InDevice, RenderMeshData);
+    // TODO: Buffer 생성하는 코드 여기에 작성
+    // SkeletalSubMeshAsset->RenderBuffer = std::make_unique<FMeshBuffer>();
+    // SkeletalSubMeshAsset->RenderBuffer->Create(InDevice, RenderMeshData);
+    // NOTE: AI가 작성한 초안이 있었지만 FMeshBuffer 리팩토링과 관련한 Merge Conflict 때문에 
+    //       주석 처리했습니다. 재구현 필요
 }
