b0b4c5e9a0f8fed9432521f6f7e0bac941e45a2e
김태현 <th2252@gmail.com>
2026-05-11 13:27:42 +0900

    fix: SkeletalMesh bone name serialize error

--- CrashEngine/Source/Engine/Mesh/Skeleton.cpp
+++ CrashEngine/Source/Engine/Mesh/Skeleton.cpp
@@ -6,7 +6,22 @@ IMPLEMENT_CLASS(USkeleton, UObject)
 void USkeleton::Serialize(FArchive& Ar)
 {
     UObject::Serialize(Ar);
-    Ar << Bones;
+    
+    // NOTE: 
+    // Bones(TArray<FBoneInfo>)는 FBoneInfo가 trivially_copyable로 판정되어 
+    // TArray의 operator<<에서 고속 복사가 일어날 수 있습니다.
+    // 하지만 FBoneInfo::operator<<는 FName을 문자열로 변환하여 저장하는 커스텀 로직을 가지므로,
+    // 반드시 개별 요소별로 루프를 돌며 직렬화해야 합니다.
+    uint32 ArrayNum = static_cast<uint32>(Bones.size());
+    Ar << ArrayNum;
+
+    if (Ar.IsLoading())
+        Bones.resize(ArrayNum);
+
+    for (auto& Bone : Bones)
+    {
+        Ar << Bone;
+    }
 }
 
 int32 USkeleton::AddBone(FName InName, int32 InParentIndex, const FTransform& InRefTransform)
