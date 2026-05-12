59676c3588f87ce2ec08c03479e2b08495e494ed
조현석 <chohs990514@gmail.com>
2026-05-12 22:14:12 +0900

    칙쇼.

--- KraftonEngine/Source/Engine/Mesh/FbxImporter.cpp
+++ KraftonEngine/Source/Engine/Mesh/FbxImporter.cpp
@@ -176,7 +176,7 @@ bool FFbxImporter::Import(const FString& FilePath)
 	Importer->Destroy();
 
 	// 임의로 m 변환. UE는 cm 단위
-	FbxSystemUnit::cm.ConvertScene(Scene);
+	FbxSystemUnit::m.ConvertScene(Scene);
 
 	FbxAxisSystem UnrealAxisSystem(FbxAxisSystem::eZAxis, FbxAxisSystem::eParityEven, FbxAxisSystem::eLeftHanded);
 	UnrealAxisSystem.DeepConvertScene(Scene);
