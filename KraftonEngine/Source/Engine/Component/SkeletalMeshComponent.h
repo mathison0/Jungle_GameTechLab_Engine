ebf1ed5e657377e898c050fe070d07440f01cd65
keonwookang0914 <kwkang0914@gmail.com>
2026-05-12 17:06:41 +0900

    refactor: SkeletalMeshComponent에 있는 대부분의 기능 SkinnedMeshcomponent로 이전

--- KraftonEngine/Source/Engine/Component/SkeletalMeshComponent.h
+++ KraftonEngine/Source/Engine/Component/SkeletalMeshComponent.h
@@ -2,8 +2,8 @@
 
 #include "SkinnedMeshComponent.h"
 
-#include "Math/Rotator.h"
-
+// SkeletalMesh 전용 render proxy만 제공하는 얇은 wrapper.
+// Skinning/bone/material/bounds 상태는 모두 USkinnedMeshComponent가 소유한다.
 class USkeletalMeshComponent : public USkinnedMeshComponent
 {
 public:
@@ -11,45 +11,6 @@ public:
 	USkeletalMeshComponent() = default;
 	~USkeletalMeshComponent() override = default;
 
-	void SetSkeletalMesh(USkeletalMesh* InMesh) override;
-
-	FMeshBuffer* GetMeshBuffer() const override;
-	FMeshDataView GetMeshDataView() const override;
-	// void UpdateWorldAABB() const override;
-
+	// Render access 섹션: SceneProxy
 	FPrimitiveSceneProxy* CreateSceneProxy() override;
-
-	void EnsureBoneEditPose();
-	void ResetBoneEditPose();
-
-	FVector GetBoneLocationByIndex(int32 BoneIndex) const;
-	FRotator GetBoneRotationByIndex(int32 BoneIndex) const;
-	FQuat GetBoneQuatByIndex(int32 BoneIndex) const;
-	FVector GetBoneScaleByIndex(int32 BoneIndex) const;
-	FTransform GetBoneLocalTransformByIndex(int32 BoneIndex) const;
-
-	void SetBoneLocationByIndex(int32 BoneIndex, const FVector& NewLocation);
-	void SetBoneRotationByIndex(int32 BoneIndex, const FRotator& NewRotation);
-	void SetBoneRotationByIndex(int32 BoneIndex, const FQuat& NewQuat);
-	void SetBoneScaleByIndex(int32 BoneIndex, const FVector& NewScale);
-	void SetBoneLocalTransformByIndex(int32 BoneIndex, const FTransform& NewLocalTransform);
-
-	void GetCurrentBoneGlobalTransforms(TArray<FTransform>& OutGlobals) const;
-	const TArray<FVertexPNCTT>& GetSkinnedVertices() const { return SkinnedVertices; }
-	uint64 GetSkinnedRevision() const { return SkinnedRevision; }
-	
-protected:
-	void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction& ThisTickFunction) override;
-
-private:
-	void InitSkinningCache();
-	void UpdateCPUSkinning();
-	void BuildBoneEditGlobalTransforms(TArray<FTransform>& OutGlobals) const;
-
-private:
-	TArray<FTransform> BoneEditLocalTransforms;
-	bool bUseBoneEditPose = false;
-
-	TArray<FVertexPNCTT> SkinnedVertices;
-	uint64 SkinnedRevision = 0;
 };
