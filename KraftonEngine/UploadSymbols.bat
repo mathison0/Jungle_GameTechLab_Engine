15b3985646aa46608b863d1b4a9ff317d97d1418
kwonhyeonsoo-goo <kkhhss4035@gmail.com>
2026-05-22 14:38:35 +0900

    Edit. symbol.bat

--- KraftonEngine/UploadSymbols.bat
+++ KraftonEngine/UploadSymbols.bat
@@ -9,7 +9,15 @@ setlocal
 set SYMBOL_SERVER=\\172.21.10.28\symbols
 
 :: 방금 빌드된 파일들(.exe, .dll, .pdb)이 있는 경로 (상대 경로 또는 절대 경로)
-set BUILD_DIR=%~dp0Bin\Debug
+set "BUILD_DIR=%~1"
+if "%BUILD_DIR%"=="" set "BUILD_DIR=%~dp0Bin\Debug\"
+if not "%BUILD_DIR:~-1%"=="\" set "BUILD_DIR=%BUILD_DIR%\"
+
+set "CONFIGURATION=%~2"
+if "%CONFIGURATION%"=="" set "CONFIGURATION=Debug"
+
+set "PLATFORM=%~3"
+if "%PLATFORM%"=="" set "PLATFORM=x64"
 
 :: 프로젝트 메타데이터
 set PRODUCT_NAME=KraftonEngine
@@ -17,7 +25,7 @@ set PRODUCT_VERSION=1.0.0
 set COMMENT="Auto Upload by Build Script"
 
 :: symstore.exe 파일이 있는 실제 경로 (Windows SDK 버전에 따라 다를 수 있음)
-set SYMSTORE_EXE="C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\symstore.exe"
+set "SYMSTORE_EXE=C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\symstore.exe"
 
 :: ==========================================
 :: 2. 업로드 실행
@@ -25,14 +33,16 @@ set SYMSTORE_EXE="C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\symstore.
 
 echo [Info] 심볼 업로드를 시작합니다...
 echo Target Directory: %BUILD_DIR%
+echo Configuration: %CONFIGURATION%
+echo Platform: %PLATFORM%
 echo Symbol Server: %SYMBOL_SERVER%
 
 :: symstore.exe 실행 (PDB 파일 업로드)
-%SYMSTORE_EXE% add /f "%BUILD_DIR%\*.pdb" /s "%SYMBOL_SERVER%" /t "%PRODUCT_NAME%" /v "%PRODUCT_VERSION%" /c %COMMENT%
+"%SYMSTORE_EXE%" add /f "%BUILD_DIR%*.pdb" /s "%SYMBOL_SERVER%" /t "%PRODUCT_NAME%" /v "%PRODUCT_VERSION%" /c %COMMENT%
 
 :: (선택) 실행 파일(.exe, .dll)도 같이 올려두면 덤프 분석이 더 수월합니다.
-:: %SYMSTORE_EXE% add /f "%BUILD_DIR%\*.exe" /s "%SYMBOL_SERVER%" /t "%PRODUCT_NAME%" /v "%PRODUCT_VERSION%" /c %COMMENT%
-:: %SYMSTORE_EXE% add /f "%BUILD_DIR%\*.dll" /s "%SYMBOL_SERVER%" /t "%PRODUCT_NAME%" /v "%PRODUCT_VERSION%" /c %COMMENT%
+:: "%SYMSTORE_EXE%" add /f "%BUILD_DIR%*.exe" /s "%SYMBOL_SERVER%" /t "%PRODUCT_NAME%" /v "%PRODUCT_VERSION%" /c %COMMENT%
+:: "%SYMSTORE_EXE%" add /f "%BUILD_DIR%*.dll" /s "%SYMBOL_SERVER%" /t "%PRODUCT_NAME%" /v "%PRODUCT_VERSION%" /c %COMMENT%
 
 :: ==========================================
 :: 3. 결과 확인
