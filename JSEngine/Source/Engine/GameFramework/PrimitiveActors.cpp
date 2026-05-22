1c5c4c6edb69eefb9775fccec3037e61fc638f05
HanYoungSeo <108516412+persona6whenrelease@users.noreply.github.com>
2026-05-22 23:15:16 +0900

    subuv test done

--- JSEngine/Source/Engine/GameFramework/PrimitiveActors.cpp
+++ JSEngine/Source/Engine/GameFramework/PrimitiveActors.cpp
@@ -137,7 +137,7 @@ void ASubUVActor::InitDefaultComponents()
 
 	auto* SubUV = AddComponent<USubUVComponent>();
 	SetRootComponent(SubUV);
-	SubUV->SetSubUV(FName("Explosion"));
+	SubUV->SetSubUV(FName("Asset/Texture/subuvtest.png"));
 	SubUV->SetSpriteSize(2.0f, 2.0f);
 	SubUV->SetFrameRate(30.f);
 }
