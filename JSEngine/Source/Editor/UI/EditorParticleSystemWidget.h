9d833c4651f99cd51db0fb375d176021deafe926
Seyoung <seyoungpark@hanyang.ac.kr>
2026-05-24 01:41:43 +0900

    feat: 에디터 내에서 emitter 생성 기능 및 emitter,module 순서 변경 기능

--- JSEngine/Source/Editor/UI/EditorParticleSystemWidget.h
+++ JSEngine/Source/Editor/UI/EditorParticleSystemWidget.h
@@ -5,6 +5,10 @@
 #include "Editor/Viewport/ParticleSystemViewportClient.h"
 #include "ImGui/imgui.h"
 
+class UParticleEmitter;
+class UParticleModule;
+class UParticleSystem;
+
 class FEditorParticleSystemWidget : public FEditorWidget
 {
 public:
@@ -34,11 +38,21 @@ private:
 	void DrawViewportPanel(const ImVec2& Size);
 	void DrawViewportMenuBar(const ImVec2& CanvasMin);
 	void DrawEmittersPanel(const ImVec2& Size);
+	void DrawEmitterContextMenu();
+	void AddDefaultEmitter();
+	void DeleteSelectedEmitter();
+	void DeleteEmitter(int32 EmitterIndex);
+	void ApplyPendingReorders();
+	void ReorderEmitter(int32 SourceIndex, int32 InsertIndex);
+	void ReorderModule(int32 EmitterIndex, int32 SourceModuleIndex, int32 InsertIndex);
+	void DrawEmitterColumn(UParticleEmitter* Emitter, int32 EmitterIndex, float ColumnHeight);
+	void DrawEmitterModuleRow(UParticleModule* Module, int32 EmitterIndex, int32 ModuleIndex, bool bRequired, float RowHeight);
 	void DrawDetailsPanel(const ImVec2& Size);
 	void DrawCurveEditorPanel(const ImVec2& Size);
 
 	FSceneViewport PreviewViewport;
 	FParticleSystemViewportClient PreviewClient;
+	UParticleSystem* ParticleSystemAsset = nullptr;
 	FName PreviewWorldHandle = FName::None;
 	FString DocumentPath;
 	bool bDirty = true;
@@ -49,6 +63,14 @@ private:
 	bool bPreviewViewportVisible = false;
 	bool bPreviewViewportRectValid = false;
 	int32 CurrentLOD = 0;
+	int32 SelectedEmitterIndex = 0;
+	int32 SelectedModuleIndex = -1;
+	int32 ContextEmitterIndex = -1;
+	int32 PendingEmitterMoveSource = -1;
+	int32 PendingEmitterMoveInsertIndex = -1;
+	int32 PendingModuleMoveEmitterIndex = -1;
+	int32 PendingModuleMoveSource = -1;
+	int32 PendingModuleMoveInsertIndex = -1;
 	float TopAreaHeight = 0.0f;
 	float TopLeftWidth = 0.0f;
 	float BottomLeftWidth = 0.0f;
